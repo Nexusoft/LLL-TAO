2cdadf817abde0b336ad90725d96cd5ff51de5cb
VidereLicet <colin@nexus.io>
2018-09-13 02:53:36 -0700

    License Contracts on Register / Operation layers

--- src/main.cpp
+++ src/main.cpp
@@ -75,21 +75,23 @@ int main(int argc, char** argv)
 
 
 
-    TAO::Register::State test = TAO::Register::State();
+    TAO::Operation::Stream test = TAO::Operation::Stream();
 
     uint256_t rand = LLC::GetRand256();
 
     test << rand;
 
-    test.print();
+    printf("memory %u\n", &rand);
+
+    //test.print();
 
     uint256_t newRand;
     test >> newRand;
 
+    printf("memory %u\n", &newRand);
+
     printf("rand1 %s\n", rand.ToString().c_str());
     printf("rand2 %s\n", newRand.ToString().c_str());
-
-
 
 
 
@@ -227,7 +229,8 @@ printf("\n");
     assert(next2.hashGenesis == hashGenesis1);
 
     //add the data to the ledger
-    next2 << (uint8_t)TAO::Operation::OP_DEBIT << hashRegister1 << hashRegister << (uint64_t)(5);
+    uint256_t hashRegRead = hashRegister1;
+    next2 << (uint8_t)TAO::Operation::OP_DEBIT << hashRegRead << hashRegister << (uint64_t)(55);
     next2.Sign(chain1.Generate(next2.nSequence, "1111"));
     if(next2.IsValid())
         next2.print();
@@ -238,7 +241,7 @@ printf("\n");
 
     TAO::Register::State stateRead2;
     if(!regDB->ReadState(hashRegister1, stateRead2))
-        return error("Failed to read state");
+        return error("Failed to read state2");
 
     stateRead2.print();
 
@@ -247,6 +250,7 @@ printf("\n");
     stateRead2 >> acct2;
 
     acct2.print();
+
 
 
     ////////////////////////////////////////////////// credit test
@@ -262,10 +266,14 @@ printf("\n");
     assert(next3.hashGenesis == hashGenesis);
 
     //add the data to the ledger
-    next3 << (uint8_t)TAO::Operation::OP_CREDIT << next2.GetHash() << uint256_t(0) << hashRegister << (uint64_t)(5);
+    next3 << (uint8_t)TAO::Operation::OP_CREDIT << next2.GetHash() << uint256_t(0) << hashRegister << (uint64_t)(55);
     next3.Sign(chain1.Generate(next1.nSequence, "1111"));
     if(next3.IsValid())
         next3.print();
+
+    legDB->WriteTx(next3.GetHash(), next3);
+
+    TAO::Operation::Execute(next3.vchLedgerData, regDB, legDB, next3.hashGenesis);
 
     if(!regDB->ReadState(hashRegister, stateRead))
         return error("Failed to read state");
@@ -274,140 +282,182 @@ printf("\n");
     stateRead >> account;
 
     account.print();
-    
-
-    //mock test some basic operations running through the ledger data
-
-
-/*
-        case TAO::Operation::OP_CREDIT:
-        {
-            uint512_t hashTx;    //read the txid and then read the OP_DEBIT to ensure that hashTo matches
-            uint256_t hashProof; //the register address that proves ownership -> equates to that register having hash owner of this genesis. Also equates to a token being used to claim ownership of the HashTo register.
-            //1. hashProof -> OP_READ register -> then get identifier -> OP_READ register address of identifier to get token total if register is a token -> divide that by total nBalance in hashProof register. Verify this to nAmount to ensure token claim is true
-            //This above is if hashProof resolves to an Account register that is not the same identifier of OP_DEBIT
-            //If same token identifier -> OP_READ register of hashProof -> check that this sigchain owns register, if so, credit nAmount
-            uint256_t hashCreditTo;    //the account to send amount to (will usually be the same as hashTo in debit. If so -> skip all token stuff)
-            uint64_t  nAmount;   //the amount to be credited
-
-            ssRead >> hashTx >> hashProof >> nAmount;
-
-            ////////////////////////// hashTx checks
-            TAO::Ledger::Transaction tx;
-            if(!txDB.ReadTx(hashTx, tx)) //fail
-
-            CDataStream ssData(tx.vchLedgerData, SER_NETWORK, LLD::DATABASE_VERSION);
-
-            uint8_t OP;
-            ssData >> OP;
-
-            //use templates for these on the operations layers
-            if(OP != TAO::Operation::OP_DEBIT) //fail
-
-            //todo deserialize these easier
-            uint256_t hashFrom;
-            ssData >> hashFrom;
-
-            uint256_t hashTo;
-            ssData >> hashTo;
-
-            uint64_t nAmount;
-            ssData >> nAmount;
-
-            uint32_t nExpires;
-            ssData >> nExpires;
-
-            if(UnifiedTimestamp() > tx.nTimestamp + nExpires) //fail if debit is expired
-
-            TAO::Register::State stateFrom;
-            if(!regDB.ReadState(hashFrom, stateFrom)) //fail
-
-            CDataStream ssState(stateFrom.GetState(), SER_NETWORK, LLD::DATABASE_VERSION);
-
-            //repetitive deserializing of objects. use templates for this on register layer
-            if(testState.nType != TAO::Register::OBJECT_ACCOUNT) //fail for now... handle other types later
-
-            TAO::Register::Account acctFrom;
-            ssState >> acctFrom;
-
-            TAO::Register State stateTo;
-            if(!regDB.ReadState(hashTo, stateTo))
-
-            CDataStream ssState(stateTo.GetState(), SER_NETWORK, LLD::DATABASE_VERSION);
-            if(stateTo.nType == TAO::Register::OBJECT_RAW) //this states that debit was to a raw register. This is data to the logical layer.
-            {
-                uint256_t hashOwner = stateTo.hashOwner;
-
-                TAO::Register::State stateOwner;
-                if(!regDB.ReadState(hashOwner, stateOWner)) //fail
-
-                if(stateOwner.nType != TAO::Register::OBJECT_TOKEN) //fail - disable anything not owned by token account for now
-
-                CDataStream ssToken(stateOwner.GetState(), SER_NETWORK, LLD::DATABASE_VERSION);
-
-                TAO::Register::Token token;
-                ssToken >> token;
-
-                TAO::Register::State stateProof;
-                if(!regDB.ReadState(hashProof, stateProof)) //fail
-
-                CDataStream ssAccount(stateProof.GetState(), SER_NETWORK, LLD::DATABASE_VERSION);
-                if(stateProof.nType != TAO::Register::OBJECT_ACCOUNT) //fail
-
-                TAO::Register::Account acctProof;
-                ssAccount >> acctProof;
-
-                if(stateProof.hashOwner != Transaction.hashGenesis) //fail - you have to own the account proof you are using
-
-                if(acctProof.nIdentifier != token.nIdentifier) //fail - your account proof is not showing valid token balance
-
-                int nTotal = token.nMaxSupply * nAmount / acctProof.nBalance; //the total you can claim is based on max supply of token vs your token balance
-
-                TAO::Register::State stateDebitTo;
-                if(!regDB.ReadState(hashDebitTo, stateDebitTo)) //fail
-
-                if(stateDebitTo.nType != TAO::Register::OBJECT_ACCOUNT) //fail
-
-                TAO::Register::Account accountTo;
-                CDataStream ssAccountTo(stateDebitTo.GetState(), SER_NETWORK, LLD::DATABASE_VERSION);
-
-                ssAccountTo >> accountTo;
-                accountTo.nBalance += nTotal;
-
-                if(stateDebitTo.hashOwner != Transaction.hashGenesis) //fail
-
-                if(hashProof != stateTo.hashOwner) //fail
-
-                stateDebitTo.SetState(accountTo.Serialize()); //need serialize in and out methods. CDataStream is too clunky here
-                regDB.WriteState(hashDebitTo, stateDebitTo); //only you can write this state since you are the owner of this register. handle in OP_WRITE
-
-                //get the amounts set and credit your claimed hashTo for credit with native hashFrom tokens
-
-
-            }
-            else if(stateTo.nType == TAO::Register::OBJECT_ACCOUNT)
-            {
-                //this is a normal credit operation. prove that you own this account and write state.
-                ssState >> accountTo;
-
-                accountTo.nBalance += nAmount;
-
-                if(stateTo.hashOwner != Transaction.hashGenesis) //fail
-
-                if(hashProof != stateTo.hashOwner) //fail
-
-                stateTo.SetState(accountTo.Serialize()); //need serialize in and out methods. CDataStream is too clunky here
-                regDB.WriteState(hashTo, stateTo); //only you can write this state since you are the owner of this register. handle in OP_WRITE operation
-            }
-        }
-
-        */
 
 
 
-    //execute the transaction operations
 
-    //need a bytes switch for object registers from byte code OBJ_ACCOUNT, OBJ_TOKEN, OBJ_ESCROW
+
+    //create the first transaction
+    TAO::Ledger::Transaction next4 = TAO::Ledger::Transaction();
+    next4.nSequence = next3.nSequence + 1;
+
+    next4.NextHash(chain.Generate(next4.nSequence + 1, "1111"));
+    next4.hashPrevTx = genesis.GetHash();
+    next4.hashGenesis = hashGenesis;
+
+    assert(next3.hashGenesis == hashGenesis);
+
+
+    uint256_t hashRegister4 = LLC::GetRand256();
+    TAO::Register::State state4 = TAO::Register::State((uint8_t)TAO::Register::OBJECT_TOKEN, hashRegister4, hashGenesis);
+
+    TAO::Register::Token token(22, 100); //token with 100 max tokens
+    state4 << token;
+
+    //add the data to the ledger
+    next4 << (uint8_t)TAO::Operation::OP_REGISTER << hashRegister4 << state4;
+    next4.Sign(chain1.Generate(next4.nSequence, "1111"));
+    if(next4.IsValid())
+        next4.print();
+
+    legDB->WriteTx(next4.GetHash(), next4);
+
+    TAO::Operation::Execute(next4.vchLedgerData, regDB, legDB, next4.hashGenesis);
+
+    TAO::Register::State stateRead4;
+    if(!regDB->ReadState(hashRegister4, stateRead4))
+        return error("Failed to read state");
+
+    TAO::Register::Token token4;
+    stateRead4 >> token4;
+
+    token4.print();
+
+
+
+
+    //create the first transaction
+    TAO::Ledger::Transaction next5 = TAO::Ledger::Transaction();
+    next5.nSequence = next3.nSequence + 1;
+
+    next5.NextHash(chain.Generate(next5.nSequence + 1, "1111"));
+    next5.hashPrevTx = genesis.GetHash();
+    next5.hashGenesis = hashGenesis;
+
+    assert(next3.hashGenesis == hashGenesis);
+
+
+    uint256_t hashRegister5 = LLC::GetRand256();
+    TAO::Register::State state5 = TAO::Register::State((uint8_t)TAO::Register::OBJECT_RAW, hashRegister5, hashRegister4);
+
+    std::string strData = "(c) copyright token ART\nTitle: Song\nAlbum: Greatest\n";
+    state5 << strData.c_str();
+
+    //add the data to the ledger
+    next5 << (uint8_t)TAO::Operation::OP_REGISTER << hashRegister5 << state5;
+    next5.Sign(chain1.Generate(next5.nSequence, "1111"));
+    if(next5.IsValid())
+        next5.print();
+
+    legDB->WriteTx(next5.GetHash(), next5);
+
+    TAO::Operation::Execute(next5.vchLedgerData, regDB, legDB, next5.hashGenesis);
+
+    TAO::Register::State stateRead5;
+    if(!regDB->ReadState(hashRegister5, stateRead5))
+        return error("Failed to read state");
+
+    char* data;
+    stateRead5 >> data;
+
+    printf("%s\n", data);
+
+
+    //create the first debit transaction
+    TAO::Ledger::Transaction next6 = TAO::Ledger::Transaction();
+    next6.nSequence = 2;
+
+    next6.NextHash(chain1.Generate(next6.nSequence + 1, "1111"));
+    next6.hashPrevTx = genesis1.GetHash();
+    next6.hashGenesis = hashGenesis;
+
+    assert(next6.hashGenesis == hashGenesis);
+
+    //create an object register account
+    TAO::Register::Account acct6(22, 50);
+    acct.print();
+
+    printf("\n");
+
+    //create a state register
+    uint256_t hashRegister6 = LLC::GetRand256();
+    TAO::Register::State state6 = TAO::Register::State((uint8_t)TAO::Register::OBJECT_ACCOUNT, hashRegister6, next6.hashGenesis);
+    state6 << acct6;
+
+    //add the data to the ledger
+    next6 << (uint8_t)TAO::Operation::OP_REGISTER << hashRegister6 << state6;
+
+    next6.Sign(chain1.Generate(next6.nSequence, "1111"));
+    if(next6.IsValid())
+        next6.print();
+
+    legDB->WriteTx(next6.GetHash(), next2);
+
+    TAO::Operation::Execute(next6.vchLedgerData, regDB, legDB, next6.hashGenesis);
+
+    TAO::Register::State stateRead6;
+    if(!regDB->ReadState(hashRegister6, stateRead6))
+        return error("Failed to read state2");
+
+    TAO::Register::Account acct66;
+    stateRead2 >> acct66;
+
+    acct66.print();
+
+
+
+    ////////////////////////////////////////////////// credit test
+
+    //create the first transaction
+    TAO::Ledger::Transaction next7 = TAO::Ledger::Transaction();
+    next7.nSequence = next7.nSequence + 1;
+
+    next7.NextHash(chain.Generate(next.nSequence + 1, "1111"));
+    next7.hashPrevTx = genesis.GetHash();
+    next7.hashGenesis = hashGenesis1;
+
+    //add the data to the ledger
+    next7 << (uint8_t)TAO::Operation::OP_DEBIT << hashRegister1 << hashRegister5 << uint64_t(20);
+    next7.Sign(chain1.Generate(next1.nSequence, "1111"));
+    if(next7.IsValid())
+        next7.print();
+
+    legDB->WriteTx(next7.GetHash(), next7);
+
+    TAO::Operation::Execute(next7.vchLedgerData, regDB, legDB, next7.hashGenesis);
+
+
+    ////////////////////////////////////////////////// credit test
+
+    //create the first transaction
+    TAO::Ledger::Transaction next8 = TAO::Ledger::Transaction();
+    next8.nSequence = next.nSequence + 1;
+
+    next8.NextHash(chain.Generate(next.nSequence + 1, "1111"));
+    next8.hashPrevTx = genesis.GetHash();
+    next8.hashGenesis = hashGenesis;
+
+    assert(next8.hashGenesis == hashGenesis);
+
+    //add the data to the ledger
+    next8 << (uint8_t)TAO::Operation::OP_CREDIT << next7.GetHash() << hashRegister6 << hashRegister << (uint64_t)(10);
+    next8.Sign(chain1.Generate(next1.nSequence, "1111"));
+    if(next8.IsValid())
+        next8.print();
+
+    legDB->WriteTx(next8.GetHash(), next3);
+
+    TAO::Operation::Execute(next8.vchLedgerData, regDB, legDB, next8.hashGenesis);
+
+    TAO::Register::State stateRead8;
+    if(!regDB->ReadState(hashRegister, stateRead8))
+        return error("Failed to read state");
+
+    TAO::Register::Account account8;
+    stateRead8 >> account8;
+
+    account8.print();
+
 
     return 0;
 }
