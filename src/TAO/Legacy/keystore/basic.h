30f98fb6023c5b9bdd7fedde7201a89fb4b66a61
VidereLicet <colin@nexus.io>
2018-12-06 18:05:07 -0700

    Basic Keystore

--- src/TAO/Legacy/keystore/basic.h
+++ src/TAO/Legacy/keystore/basic.h
@@ -14,60 +14,10 @@ ________________________________________________________________________________
 #ifndef NEXUS_TAO_LEGACY_KEYSTORE_BASIC_H
 #define NEXUS_TAO_LEGACY_KEYSTORE_BASIC_H
 
-#include <TAO/Legacy/types/base.h>
+#include <TAO/Legacy/keystore/base.h>
 
 namespace Legacy
 {
-
-    /** An abstract base class for key stores */
-    class CKeyStore
-    {
-    protected:
-
-        /** Internal recursive mutex for locks. **/
-        mutable std::recursive_mutex cs_KeyStore;
-
-    public:
-        virtual ~CKeyStore() {}
-
-
-        // abstract method defintions
-        virtual bool AddKey(const ECKey& key) = 0;
-        virtual bool HaveKey(const NexusAddress &address) const = 0;
-        virtual bool GetKey(const NexusAddress &address, ECKey& keyOut) const = 0;
-        virtual void GetKeys(std::set<NexusAddress> &setAddress) const = 0;
-        virtual bool AddCScript(const CScript& redeemScript) = 0;
-        virtual bool HaveCScript(const uint256_t &hash) const = 0;
-        virtual bool GetCScript(const uint256_t &hash, CScript& redeemScriptOut) const = 0;
-
-
-        /** Get Pub Key
-         *
-         *  Get the public key from the given nexus address.
-         *
-         *  @param[in] address The address to get public key for.
-         *  @param[out] vchPubKeyOut The public key to return.
-         *
-         *  @return true if the public key was extracted.
-         *
-         **/
-        virtual bool GetPubKey(const NexusAddress &address, std::vector<uint8_t>& vchPubKeyOut) const;
-
-
-        /** Get Secret
-         *
-         *  Get the secret key from a nexus address.
-         *
-         *  @param[in] address The address to get secret key for.
-         *  @param[out] vchSecret The secret key to return.
-         *  @param[in] fCompressed Flag to determine if key should be in compressed form.
-         *
-         *  @return true if the secret key was extracted.
-         *
-         **/
-        virtual bool GetSecret(const NexusAddress &address, CSecret& vchSecret, bool &fCompressed) const;
-    };
-
 
     /** Basic key store, that keeps keys in an address->secret map */
     class CBasicKeyStore : public CKeyStore
@@ -75,7 +25,7 @@ namespace Legacy
     protected:
 
         /** Internal map to hold list of keys by address. **/
-        std::map<NexusAddress, std::pair<CSecret, bool> > mapKeys;
+        std::map<NexusAddress, std::pair<LLC::CSecret, bool> > mapKeys;
 
 
         /** Internal map to hold scripts by hash. **/
@@ -106,15 +56,7 @@ namespace Legacy
          *  @return true if the key was found.
          *
          **/
-        bool HaveKey(const NexusAddress &address) const
-        {
-            bool result;
-            {
-                LOCK(cs_KeyStore);
-                result = (mapKeys.count(address) > 0);
-            }
-            return result;
-        }
+        bool HaveKey(const NexusAddress &address) const;
 
 
         /** Get Keys
@@ -124,19 +66,7 @@ namespace Legacy
          *  @param[out] setAddresses The addresses of keys in keystore.
          *
          **/
-        void GetKeys(std::set<NexusAddress> &setAddress) const
-        {
-            setAddress.clear();
-            {
-                LOCK(cs_KeyStore);
-                KeyMap::const_iterator mi = mapKeys.begin();
-                while (mi != mapKeys.end())
-                {
-                    setAddress.insert((*mi).first);
-                    mi++;
-                }
-            }
-        }
+        void GetKeys(std::set<NexusAddress> &setAddress) const;
 
 
         /** Get Key
@@ -149,20 +79,7 @@ namespace Legacy
          *  @return true if the key was found.
          *
          **/
-        bool GetKey(const NexusAddress &address, LLC::ECKey &keyOut) const
-        {
-            {
-                LOCK(cs_KeyStore);
-                KeyMap::const_iterator mi = mapKeys.find(address);
-                if (mi != mapKeys.end())
-                {
-                    keyOut.Reset();
-                    keyOut.SetSecret((*mi).second.first, (*mi).second.second);
-                    return true;
-                }
-            }
-            return false;
-        }
+        bool GetKey(const NexusAddress &address, LLC::ECKey &keyOut) const;
 
 
         /** Add CScript
