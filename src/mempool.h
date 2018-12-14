ff31cd3669d718961ddbce186467616390c79c96
VidereLicet <colin@nexus.io>
2018-12-14 15:36:05 -0700

    Cleanup Block / Mempool

---
+++ src/mempool.h
@@ -0,0 +1,24 @@
+/*__________________________________________________________________________________________
+
+			(c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+
+			(c) Copyright The Nexus Developers 2014 - 2018
+
+			Distributed under the MIT software license, see the accompanying
+			file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+			"ad vocem populi" - To the Voice of the People
+
+____________________________________________________________________________________________*/
+
+#ifndef NEXUS_TAO_LEDGER_TYPES_MEMPOOL_H
+#define NEXUS_TAO_LEDGER_TYPES_MEMPOOL_H
+
+#include <LLC/types/uint1024.h>
+
+namespace TAO::Ledger
+{
+
+}
+
+#endif
