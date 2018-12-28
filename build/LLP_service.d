7f8aaba78d7f13baaddac7bc61351f2eed0f509d
VidereLicet <colin@nexus.io>
2018-12-28 12:32:10 -0700

    Add login required

--- build/LLP_service.d
+++
@@ -1,9 +0,0 @@
-build/LLP_service.o: src/LLP/service.cpp \
- /home/viz/Nexus/Branches/TAO-Master/src/LLP/include/service.h \
- /home/viz/Nexus/Branches/TAO-Master/src/LLP/include/netaddr.h \
- /home/viz/Nexus/Branches/TAO-Master/src/LLP/include/network.h \
- /home/viz/Nexus/Branches/TAO-Master/src/Util/templates/serialize.h \
- /home/viz/Nexus/Branches/TAO-Master/src/LLC/types/uint1024.h \
- /home/viz/Nexus/Branches/TAO-Master/src/Util/include/allocators.h \
- /home/viz/Nexus/Branches/TAO-Master/src/Util/include/debug.h \
- /home/viz/Nexus/Branches/TAO-Master/src/LLP/include/hosts.h
