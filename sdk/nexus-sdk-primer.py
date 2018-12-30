cbf658e1eaea0b5917efe2853affdcef8a199b87
Dino Farinacci <farinacci@gmail.com>
2018-12-29 17:48:27 -0800

    Add width so if all output lines need to be column adjusted, you can just change one line of code.

--- sdk/nexus-sdk-primer.py
+++ sdk/nexus-sdk-primer.py
@@ -5,7 +5,8 @@
 # API. If you want more details and want to see more interactions and
 # return response detail, use LLL-TAO/sdk/nexus-sdk-test.py.
 #
-# If you want a bit more detail just uncomment "#print json" lines.
+# If you want a bit more detail in this program, just uncomment "#print json"
+# lines.
 #
 # Usage: python nexus-sdk-primer.py
 #
@@ -13,29 +14,31 @@
 
 import nexus_sdk
 
-print "Initialize Nexus SDK ...".ljust(40),
-primer = nexus_sdk.sdk_init("primer", "primer", "1234")
+width = 40
+
+print "Initialize Nexus SDK ...".ljust(width),
+primer = nexus_sdk.sdk_init("primer", "primer-password", "1234")
 print "succeeded"
 
-print "Create username 'primer' ...".ljust(40),
+print "Create username 'primer' ...".ljust(width),
 json = primer.nexus_accounts_create()
 good = "failed" if json.has_key("error") else "succeeded"
 print good
 #print json
 
-print "Login user 'primer' ...".ljust(40),
+print "Login user 'primer' ...".ljust(width),
 json = primer.nexus_accounts_login()
 good = "failed" if json.has_key("error") else "succeeded"
 print good
 #print json
 
-print "Show transactions for user 'primer' ...".ljust(40),
+print "Show transactions for user 'primer' ...".ljust(width),
 json= primer.nexus_accounts_transactions()
 good = "failed" if json.has_key("error") else "succeeded"
 print good
 #print json
 
-print "Logout user 'primer' ...".ljust(40),
+print "Logout user 'primer' ...".ljust(width),
 json= primer.nexus_accounts_logout()
 good = "failed" if json.has_key("error") else "succeeded"
 print good
