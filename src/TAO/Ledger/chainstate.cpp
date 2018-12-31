3d334414ed34dc14ea29dc30063adbd763c7ce77
VidereLicet <colin@nexus.io>
2018-12-30 23:09:09 -0700

    remove tabs chainstate

--- src/TAO/Ledger/chainstate.cpp
+++ src/TAO/Ledger/chainstate.cpp
@@ -1,13 +1,13 @@
 /*__________________________________________________________________________________________
 
-			(c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+            (c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
 
-			(c) Copyright The Nexus Developers 2014 - 2018
+            (c) Copyright The Nexus Developers 2014 - 2018
 
-			Distributed under the MIT software license, see the accompanying
-			file COPYING or http://www.opensource.org/licenses/mit-license.php.
+            Distributed under the MIT software license, see the accompanying
+            file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-			"ad vocem populi" - To The Voice of The People
+            "ad vocem populi" - To The Voice of The People
 
 ____________________________________________________________________________________________*/
 
@@ -17,30 +17,30 @@ ________________________________________________________________________________
 namespace TAO::Ledger
 {
 
-	/* The best block height in the chain. */
-	uint32_t ChainState::nBestHeight = 0;
+    /* The best block height in the chain. */
+    uint32_t ChainState::nBestHeight = 0;
 
 
-	/* The best hash in the chain. */
-	uint1024_t ChainState::hashBestChain = 0;
+    /* The best hash in the chain. */
+    uint1024_t ChainState::hashBestChain = 0;
 
 
-	/* The best trust in the chain. */
-	uint64_t ChainState::nBestChainTrust = 0;
+    /* The best trust in the chain. */
+    uint64_t ChainState::nBestChainTrust = 0;
 
 
-	/* Flag to tell if initial blocks are downloading. */
-	bool ChainState::Synchronizing()
-	{
-		return false;
-	}
+    /* Flag to tell if initial blocks are downloading. */
+    bool ChainState::Synchronizing()
+    {
+        return false;
+    }
 
 
-	/** The best block in the chain. **/
-	BlockState ChainState::stateBest;
+    /** The best block in the chain. **/
+    BlockState ChainState::stateBest;
 
 
-	/** The genesis block in the chain. **/
-	BlockState ChainState::stateGenesis;
+    /** The genesis block in the chain. **/
+    BlockState ChainState::stateGenesis;
 
 }
