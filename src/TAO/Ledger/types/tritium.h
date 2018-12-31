7f8e6761ced55735af4b2dbbd1cb3f76d330792a
VidereLicet <colin@nexus.io>
2018-12-30 23:15:10 -0700

    remove tabs tritium

--- src/TAO/Ledger/types/tritium.h
+++ src/TAO/Ledger/types/tritium.h
@@ -1,13 +1,13 @@
 /*__________________________________________________________________________________________
 
-			(c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+            (c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
 
-			(c) Copyright The Nexus Developers 2014 - 2018
+            (c) Copyright The Nexus Developers 2014 - 2018
 
-			Distributed under the MIT software license, see the accompanying
-			file COPYING or http://www.opensource.org/licenses/mit-license.php.
+            Distributed under the MIT software license, see the accompanying
+            file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-			"ad vocem populi" - To the Voice of the People
+            "ad vocem populi" - To the Voice of the People
 
 ____________________________________________________________________________________________*/
 
