622cf02979a69da47254a05c3274df95f4b1c552
VidereLicet <colin@nexus.io>
2018-12-30 22:32:16 -0700

    Code cleanup

--- src/TAO/Ledger/trustkey.cpp
+++ src/TAO/Ledger/trustkey.cpp
@@ -20,124 +20,123 @@ ________________________________________________________________________________
 
 namespace TAO::Ledger
 {
-        
-        /* Initializes a null Trust Key. */
-        TrustKey::TrustKey() 
-        { 
-        	SetNull(); 
-        }
+
+    /* Initializes a null Trust Key. */
+    TrustKey::TrustKey()
+    {
+        SetNull();
+    }
 
 
-        /* Initializes a Trust Key. */
-        TrustKey::TrustKey(std::vector<uint8_t> vchPubKeyIn, uint1024_t hashBlockIn, uint512_t hashTxIn, int32_t nTimeIn)
+    /* Initializes a Trust Key. */
+    TrustKey::TrustKey(std::vector<uint8_t> vchPubKeyIn, uint1024_t hashBlockIn, uint512_t hashTxIn, int32_t nTimeIn)
+    {
+        SetNull();
+
+        nVersion               = 1;
+        vchPubKey              = vchPubKeyIn;
+        hashGenesisBlock       = hashBlockIn;
+        hashGenesisTx          = hashTxIn;
+        nGenesisTime           = nTimeIn;
+    }
+
+
+    /* Set the Trust Key data to Null (uninitialized) values. */
+    void TrustKey::SetNull()
+    {
+        nVersion             = 1;
+        hashGenesisBlock     = 0;
+        hashGenesisTx        = 0;
+        nGenesisTime         = 0;
+
+        hashPrevBlocks.clear();
+        vchPubKey.clear();
+    }
+
+
+    /* Retrieve how old the Trust Key is at a given point in time. */
+    uint64_t TrustKey::GetAge(const uint32_t nTime) const
+    {
+        if (nTime < nGenesisTime)
         {
-            SetNull();
-
-            nVersion               = 1;
-            vchPubKey              = vchPubKeyIn;
-            hashGenesisBlock       = hashBlockIn;
-            hashGenesisTx          = hashTxIn;
-            nGenesisTime           = nTimeIn;
+        	return 0;
         }
-
-
-        /* Set the Trust Key data to Null (uninitialized) values. */
-        void TrustKey::SetNull()
+        else
         {
-            nVersion             = 1;
-            hashGenesisBlock     = 0;
-            hashGenesisTx        = 0;
-            nGenesisTime         = 0;
-
-			hashPrevBlocks.clear();
-            vchPubKey.clear();
+        	return (uint64_t)(nTime - nGenesisTime);
         }
+    }
 
 
-        /* Retrieve how old the Trust Key is at a given point in time. */
-        uint64_t TrustKey::GetAge(const uint32_t nTime) const
-		{ 
-			if (nTime < nGenesisTime)
-			{
-				return 0;
-			}
-			else
-			{
-				return (uint64_t)(nTime - nGenesisTime); 
-			}
-		}        
+    /* Retrieve the time since this Trust Key last generated a Proof of Stake block. */
+    uint64_t TrustKey::GetBlockAge(const uint32_t nTime) const
+    {
+        //TODO requires implementation
+        return 0; //temp until implementation completed
+    }
 
 
-        /* Retrieve the time since this Trust Key last generated a Proof of Stake block. */
-        uint64_t TrustKey::GetBlockAge(const uint32_t nTime) const
-	    {
-//TODO requires implementation
-	    	return 0; //temp until implementation completed
-	    }
+    /* Determine if a key is expired at a given point in time. */
+    bool TrustKey::IsExpired(uint32_t nTime) const
+    {
+        if (GetBlockAge(nTime) > TRUST_KEY_EXPIRE)
+        	return true;
+
+        return false;
+    }
 
 
-		/* Determine if a key is expired at a given point in time. */
-		bool TrustKey::IsExpired(uint32_t nTime) const
-		{
-			if (GetBlockAge(nTime) > TRUST_KEY_EXPIRE)
-				return true;
+    /* Check the Genesis Transaction of this Trust Key. */
+    bool TrustKey::CheckGenesis(TritiumBlock block) const
+    {
+        /* Invalid if Null. */
+        if(IsNull())
+            return false;
 
-			return false;
-		}
+        /* Trust Keys must be created from only Proof of Stake Blocks. */
+        if(!block.IsProofOfStake())
+            return debug::error("TrustKey::CheckGenesis() : genesis has to be proof of stake");
 
+        /* Trust Key Timestamp must be the same as Genesis Key Block Timestamp. */
+        if(nGenesisTime != block.nTime)
+            return debug::error("TrustKey::CheckGenesis() : genesis time mismatch");
 
-        /* Check the Genesis Transaction of this Trust Key. */
-        bool TrustKey::CheckGenesis(TritiumBlock block) const
-        {
-            /* Invalid if Null. */
-            if(IsNull())
-                return false;
+        /* Check the genesis block hash. */
+        if(block.GetHash() != hashGenesisBlock)
+            return debug::error("TrustKey::CheckGenesis() : genesis hash mismatch");
 
-            /* Trust Keys must be created from only Proof of Stake Blocks. */
-            if(!block.IsProofOfStake())
-                return debug::error("TrustKey::CheckGenesis() : genesis has to be proof of stake");
+        /* Genesis Transaction must match Trust Key Genesis Hash. */
+        if (block.vtx.size() == 0)
+            return debug::error("TrustKey::CheckGenesis() : genesis coinstake not present");
 
-            /* Trust Key Timestamp must be the same as Genesis Key Block Timestamp. */
-            if(nGenesisTime != block.nTime)
-                return debug::error("TrustKey::CheckGenesis() : genesis time mismatch");
+        uint512_t txHash = block.vtx[0].second;
 
-            /* Check the genesis block hash. */
-            if(block.GetHash() != hashGenesisBlock)
-                return debug::error("TrustKey::CheckGenesis() : genesis hash mismatch");
+        if(txHash != hashGenesisTx)
+            return debug::error("TrustKey::CheckGenesis() : genesis coinstake hash mismatch");
 
-            /* Genesis Transaction must match Trust Key Genesis Hash. */
-            if (block.vtx.size() == 0)
-                return debug::error("TrustKey::CheckGenesis() : genesis coinstake not present");
-
-            uint512_t txHash = block.vtx[0].second;
-
-            if(txHash != hashGenesisTx)
-                return debug::error("TrustKey::CheckGenesis() : genesis coinstake hash mismatch");
-
-            return true;
-        }
+        return true;
+    }
 
 
-        /* Generate a string representation of this Trust Key. */
-        std::string TrustKey::ToString()
-        {
-            uint576_t cKey;
-            cKey.SetBytes(vchPubKey);
+    /* Generate a string representation of this Trust Key. */
+    std::string TrustKey::ToString()
+    {
+        uint576_t cKey;
+        cKey.SetBytes(vchPubKey);
 
-            return debug::strprintf("hash=%s, key=%s, genesis=%s, tx=%s, time=%u, age=%u", 
-                                    GetHash().ToString().c_str(), 
-                                    cKey.ToString().c_str(), 
-                                    hashGenesisBlock.ToString().c_str(), 
-                                    hashGenesisTx.ToString().c_str(), 
-                                    nGenesisTime, 
-                                    GetAge(runtime::unifiedtimestamp()));
-        }
+        return debug::strprintf("hash=%s, key=%s, genesis=%s, tx=%s, time=%u, age=%u",
+                                GetHash().ToString().c_str(),
+                                cKey.ToString().c_str(),
+                                hashGenesisBlock.ToString().c_str(),
+                                hashGenesisTx.ToString().c_str(),
+                                nGenesisTime,
+                                GetAge(runtime::unifiedtimestamp()));
+    }
 
 
-        /* Print a string representation of this Trust Key to the debug::log. */
-        void TrustKey::Print()
-        {
-            debug::log(0, "TrustKey(%s)", ToString().c_str());
-        }
-
+    /* Print a string representation of this Trust Key to the debug::log. */
+    void TrustKey::Print()
+    {
+        debug::log(0, "TrustKey(%s)", ToString().c_str());
+    }
 }
--- src/TAO/Ledger/types/trustkey.h
+++ src/TAO/Ledger/types/trustkey.h
@@ -27,63 +27,63 @@ namespace TAO::Ledger
 	class TritiumBlock;
 
 
-    /** @class CTrustKey
-     *
-     *  Class to Store a legacy Trust Key and its Interest Rate. 
-     *
-     **/
+	/** @class TrustKey
+	*
+	*  Class to Store a legacy Trust Key and its Interest Rate.
+	*
+	**/
 	class TrustKey
 	{
 	public:
-       /** The Public Key associated with this Trust Key. **/
-        std::vector<uint8_t> vchPubKey;
+		/** The Public Key associated with this Trust Key. **/
+		std::vector<uint8_t> vchPubKey;
 
-        /** Trust Key version **/
-        int32_t nVersion;
 
-        /** Hash of legacy block containing the Genesis transaction for this Trust Key **/
-        uint1024_t  hashGenesisBlock;
+		/** Trust Key version **/
+		int32_t nVersion;
 
-        /** Hash of the Genesis transaction for this Trust Key **/
-        uint512_t   hashGenesisTx;
 
-        /** Timestamp of the Genesis transaction for this Trust Key **/
-        int32_t nGenesisTime;
+		/** Hash of legacy block containing the Genesis transaction for this Trust Key **/
+		uint1024_t  hashGenesisBlock;
+
+
+		/** Hash of the Genesis transaction for this Trust Key **/
+		uint512_t   hashGenesisTx;
+
+
+		/** Timestamp of the Genesis transaction for this Trust Key **/
+		int32_t nGenesisTime;
+
 
 		/** Previous Blocks Vector to store list of blocks for this Trust Key. **/
 		mutable std::vector<uint1024_t> hashPrevBlocks;
 
 
-        /** Constructor
-         *
-         *  Initializes a null Trust Key.
-         *
-         **/
-        TrustKey();
+		/** Constructor
+		 *
+		 *  Initializes a null Trust Key.
+		 *
+		 **/
+		TrustKey();
 
 
-        /** Constructor
-         *
-         *  Initializes a Trust Key.
-         *
-         *  @param[in] vchPubKeyIn Public key for this Trust Key
-         *
-         *  @param[in] hashBlockIn The hashGenesisBlock value for this Trust Key
-         *
-         *  @param[in] hashTxIn The hashGenesisTx value for this Trust Key
-         *
-         *  @param[in] nTimeIn The nGenesisTime value for this Trust Key
-         *
-         **/
-        TrustKey(const std::vector<uint8_t> vchPubKeyIn, const uint1024_t hashBlockIn, const uint512_t hashTxIn, const int32_t nTimeIn);
+		/** Constructor
+		 *
+		 *  Initializes a Trust Key.
+		 *
+		 *  @param[in] vchPubKeyIn Public key for this Trust Key
+		 *  @param[in] hashBlockIn The hashGenesisBlock value for this Trust Key
+		 *  @param[in] hashTxIn The hashGenesisTx value for this Trust Key
+		 *  @param[in] nTimeIn The nGenesisTime value for this Trust Key
+		 *
+		 **/
+		TrustKey(const std::vector<uint8_t> vchPubKeyIn, const uint1024_t hashBlockIn, const uint512_t hashTxIn, const int32_t nTimeIn);
 
 
-        /* Define Serialization/Deserialization for Trust Key */
+		/* Define Serialization/Deserialization for Trust Key */
 		IMPLEMENT_SERIALIZE
 		(
-			READWRITE(this->nVersion);
-			nSerVersion = this->nVersion;
-
+			READWRITE(nVersion);
 			READWRITE(vchPubKey);
 			READWRITE(hashGenesisBlock);
 			READWRITE(hashGenesisTx);
@@ -91,41 +91,41 @@ namespace TAO::Ledger
 		)
 
 
-        /** SetNull
-         *
-         *  Set the Trust Key data to Null (uninitialized) values. 
-         *
-         **/
+		/** SetNull
+		 *
+		 *  Set the Trust Key data to Null (uninitialized) values.
+		 *
+		 **/
 		void SetNull();
 
 
-        /** GetAge 
-         *
-         *  Retrieve how old the Trust Key is at a given point in time. 
-         *
-         *  @param[in] nTime The timestamp of the end time for determining age
-         *
-         *  @return Elapsed time between Trust Key Genesis timestamp and the requested nTime, or 0 if nTime < nGenesisTime
-         *
-         **/
-        uint64_t GetAge(const uint32_t nTime) const; 
+		/** GetAge
+		 *
+		 *  Retrieve how old the Trust Key is at a given point in time.
+		 *
+		 *  @param[in] nTime The timestamp of the end time for determining age
+		 *
+		 *  @return Elapsed time between Trust Key Genesis timestamp and the requested nTime, or 0 if nTime < nGenesisTime
+		 *
+		 **/
+		uint64_t GetAge(const uint32_t nTime) const;
 
 
-        /** GetBlockAge 
-         *
-         *  Retrieve the time since this Trust Key last generated a Proof of Stake block. 
-         *
-         *  @param[in] nTime The timestamp of the end time for determining age
-         *
-         *  @return Elapsed time between last block genereated and the requested nTime
-         *
-         **/
+		/** GetBlockAge
+		 *
+		 *  Retrieve the time since this Trust Key last generated a Proof of Stake block.
+		 *
+		 *  @param[in] nTime The timestamp of the end time for determining age
+		 *
+		 *  @return Elapsed time between last block genereated and the requested nTime
+		 *
+		 **/
 		uint64_t GetBlockAge(const uint32_t nTime) const;
 
 
 		/** GetHash
 		 *
-		 *  Retrieve hash of a Trust Key to Verify the Key's Root. 
+		 *  Retrieve hash of a Trust Key to Verify the Key's Root.
 		 *
 		 *  @return Hash of Public Key associated with this Trust Key
 		 *
@@ -135,7 +135,7 @@ namespace TAO::Ledger
 
 		/** IsExpired
 		 *
-		 *  Determine if a key is expired at a given point in time. 
+		 *  Determine if a key is expired at a given point in time.
 		 *  Expiration only applies to nVersion=4 or earlier trust keys, though this method may be called for any.
 		 *
 		 *  @param[in] nTime The timestamp of the point in time to check for expiration
@@ -146,46 +146,46 @@ namespace TAO::Ledger
 		bool IsExpired(uint32_t nTime) const;
 
 
-        /** IsNull
-         *
-         *  Check if the Trust Key data contains Null (uninitialized) values. 
-         *
-         *  @return true if Trust Key uninitialized, false otherwise
-         *
-         */
-        inline bool IsNull() const
-        {
-            return (hashGenesisBlock == 0 && hashGenesisTx == 0 && nGenesisTime == 0 && vchPubKey.empty());
-        }
+		/** IsNull
+		 *
+		 *  Check if the Trust Key data contains Null (uninitialized) values.
+		 *
+		 *  @return true if Trust Key uninitialized, false otherwise
+		 *
+		 */
+		inline bool IsNull() const
+		{
+		    return (hashGenesisBlock == 0 && hashGenesisTx == 0 && nGenesisTime == 0 && vchPubKey.empty());
+		}
 
 
-        /** CheckGenesis
-         *
-         *  Check the Genesis Transaction of this Trust Key.
-         *
-         *  @param[in] block The block containing the Genesis Coinstake transaction for this Trust Key
-         *
-         *  @return true if block contains valid Genesis transaction for this Trust Key, false otherwise
-         *
-         */
+		/** CheckGenesis
+		 *
+		 *  Check the Genesis Transaction of this Trust Key.
+		 *
+		 *  @param[in] block The block containing the Genesis Coinstake transaction for this Trust Key
+		 *
+		 *  @return true if block contains valid Genesis transaction for this Trust Key, false otherwise
+		 *
+		 */
 		bool CheckGenesis(const TritiumBlock block) const;
 
 
-        /** ToString
-         *
-         *  Generate a string representation of this Trust Key.
-         *
-         *  @return data for this Trust Key as string
-         *
-         */
+		/** ToString
+		 *
+		 *  Generate a string representation of this Trust Key.
+		 *
+		 *  @return data for this Trust Key as string
+		 *
+		 */
 		std::string ToString();
 
 
-        /** Print
-         *
-         *  Print a string representation of this Trust Key to the debug::log.
-         *
-         */
+		/** Print
+		 *
+		 *  Print a string representation of this Trust Key to the debug::log.
+		 *
+		 */
 		void Print();
 
 	};
