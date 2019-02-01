ca708aee24024651cc9fde1b1f5b8456c4d4ae33
VidereLicet <colin@nexus.io>
2019-01-31 18:11:58 -0700

    Add events to unified time.

--- src/LLP/time.cpp
+++ src/LLP/time.cpp
@@ -18,8 +18,51 @@ namespace LLP
     /* Virtual Functions to Determine Behavior of Message LLP. */
     void TimeNode::Event(uint8_t EVENT, uint32_t LENGTH)
     {
-        return;
+        /** Handle any DDOS Packet Filters. **/
+        if(EVENT == EVENT_HEADER)
+        {
+            if(fDDOS)
+            {
+                Packet PACKET   = this->INCOMING;
+
+                if(PACKET.HEADER == TIME_DATA)
+                    DDOS->Ban();
+
+                if(PACKET.HEADER == ADDRESS_DATA)
+                    DDOS->Ban();
+
+                if(PACKET.HEADER == TIME_OFFSET)
+                    DDOS->Ban();
+
+                if(PACKET.HEADER == GET_OFFSET && PACKET.LENGTH > 4)
+                    DDOS->Ban();
+
+                if(DDOS->Banned())
+                    return;
+
+            }
+        }
+
+
+        /** Handle for a Packet Data Read. **/
+        if(EVENT == EVENT_PACKET)
+            return;
+
+
+        /** On Generic Event, Broadcast new block if flagged. **/
+        if(EVENT == EVENT_GENERIC)
+            return;
+
+        /** On Connect Event, Assign the Proper Daemon Handle. **/
+        if(EVENT == EVENT_CONNECT)
+            return;
+
+        /** On Disconnect Event, Reduce the Connection Count for Daemon **/
+        if(EVENT == EVENT_DISCONNECT)
+            return;
+
     }
+
 
     /* Main message handler once a packet is recieved. */
     bool TimeNode::ProcessPacket()
