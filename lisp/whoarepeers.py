6e80dbfefd21e04161290ae9598733717923dd53
Dino Farinacci <farinacci@gmail.com>
2019-03-07 11:16:30 -0800

    Add 2 aliases, whoarepeers and ltr. The alias whoarepeers will do a getpeerinfo and lookup the peer address in the mapping database to get a name associated with the nexus node. If not an EID, it will display "underlay". Alias ltr is a shorthand for "python ltr.pyo" which is coming in the next lispers.net release.

---
+++ lisp/whoarepeers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#
+# whoarepeers.py
+#
+# Ask getpeerinfo to get peers, and if an EID peer, lig the EID so the
+# LISP mapping system can return an rloc-name. This gives an operator a rough
+# idea who owns and manages the nexus node.
+#
+
+import commands
+
+peerinfo = "/nexus/nexus getpeerinfo | egrep addr"
+lig = ('python -O /lispers.net/lisp-lig.pyo "[200]{}" to node1.nexusoft.io' + \
+  "| egrep rloc-name")
+
+peers = commands.getoutput(peerinfo)
+peers = peers.split("\n")
+
+for peer in peers:
+    p = peer.split(":")[1]
+    p = p.replace(' "', '')
+    pp = (p + " ...").ljust(25)
+    print "Found peer {}".format(pp),
+
+    if (peer[0:4] == "240." or peer[0:4] == "fe::"):
+        out = commands.getoutput(lig.format(p))
+        out = out.split()
+        print out[-1]
+    else:
+        print "underlay"
+    #endif
+#endfor
+
+
