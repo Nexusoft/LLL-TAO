5552b48161a5ec6dde621c458b0711142916c4ed
VidereLicet <colin@nexus.io>
2019-03-15 10:39:53 -0700

    Live tests for Falcon Key

--- tests/unit/main.cpp
+++ tests/unit/main.cpp
@@ -3,136 +3,9 @@
 
 #include <Util/include/runtime.h>
 
-#include <LLC/falcon/falcon.h>
+#include <LLC/include/flkey.h>
 
 #include <TAO/Ledger/types/sigchain.h>
-
-class FLKey
-{
-    /* The contained falcon public key. */
-    std::vector<uint8_t> vchPubKey;
-
-    /* the contained falcon private key. */
-    std::vector<uint8_t> vchPrivKey;
-
-
-    uint32_t nLog;
-
-
-    uint32_t nSigSize;
-
-public:
-    FLKey()
-    : vchPubKey()
-    , vchPrivKey()
-    , nLog(9)
-    , nSigSize(0)
-    {
-        if(nLog == 9)
-            nSigSize = 1025;
-        else if(nLog == 10)
-            nSigSize = 2049;
-        else
-            throw std::runtime_error("Invalid log input for FL Key");
-    }
-
-    void SetPubKey(const std::vector<uint8_t>& vchPubKeyIn)
-    {
-        vchPubKey = vchPubKeyIn;
-    }
-
-
-    std::vector<uint8_t> GetPubKey()
-    {
-        return vchPubKey;
-    }
-
-
-    void SetSecret(const std::vector<uint8_t>& vchSecret)
-    {
-        falcon_keygen* fk = falcon_keygen_new(nLog, 0);
-
-        falcon_keygen_set_seed(fk, &vchSecret[0], vchSecret.size(), 1);
-
-        size_t privkey_size = falcon_keygen_max_privkey_size(fk);
-        size_t pubkey_size  = falcon_keygen_max_pubkey_size(fk);
-
-        vchPubKey.resize(pubkey_size);
-        vchPrivKey.resize(privkey_size);
-
-        int ret = falcon_keygen_make(fk, FALCON_COMP_STATIC, &vchPrivKey[0], &privkey_size, &vchPubKey[0], &pubkey_size);
-
-        vchPrivKey.resize(privkey_size);
-        vchPubKey.resize(pubkey_size);
-
-        falcon_keygen_free(fk);
-    }
-
-
-    bool Sign(const std::vector<uint8_t>& vchData, std::vector<uint8_t>& vchSignature)
-    {
-        vchSignature.clear();
-        vchSignature.resize(nSigSize);
-
-        falcon_sign* fs = falcon_sign_new();
-
-        if(falcon_sign_set_private_key(fs, &vchPrivKey[0], vchPrivKey.size()) != 1)
-        {
-            falcon_sign_free(fs);
-            return false;
-        }
-
-        std::vector<uint8_t> nonce(40);
-        if(falcon_sign_start(fs, &nonce[0]) != 1)
-        {
-            falcon_sign_free(fs);
-            return false;
-        }
-
-        falcon_sign_update(fs, &vchData[0], vchData.size());
-
-        nSigSize = falcon_sign_generate(fs, &vchSignature[0], vchSignature.size(), FALCON_COMP_STATIC);
-
-        //debug::log(0, HexStr(vchSignature.begin(), vchSignature.end()));
-        vchSignature.resize(nSigSize);
-        //debug::log(0, HexStr(vchSignature.begin(), vchSignature.end()));
-
-        //debug::log(0, "SigSize ", nSigSize);
-
-        vchSignature.insert(vchSignature.end(), nonce.begin(), nonce.end());
-
-        falcon_sign_free(fs);
-
-
-
-        return true;
-    }
-
-    bool Verify(const std::vector<uint8_t>& vchData, const std::vector<uint8_t>& vchSignature)
-    {
-        falcon_vrfy* fv = falcon_vrfy_new();
-
-        if(falcon_vrfy_set_public_key(fv, &vchPubKey[0], vchPubKey.size()) != 1)
-        {
-            falcon_vrfy_free(fv);
-            return debug::error(FUNCTION, "failed to et pukey");
-        }
-
-        nSigSize = vchSignature.size() - 40;
-        falcon_vrfy_start(fv, &vchSignature[nSigSize], 40);
-
-        falcon_vrfy_update(fv, &vchData[0], vchData.size());
-
-        int nResult = falcon_vrfy_verify(fv, &vchSignature[0], nSigSize);
-        if(nResult != 1)
-        {
-            falcon_vrfy_free(fv);
-            return debug::error(FUNCTION, "invalid signature ", nResult);
-        }
-
-        return true;
-    }
-};
 
 
 std::atomic<uint64_t> nVerified;
@@ -147,7 +20,7 @@ void Verifier()
 {
     while(true)
     {
-        FLKey key2;
+        LLC::FLKey key2;
         key2.SetPubKey(vchPubKey);
         if(!key2.Verify(vchMessage, vchSignature))
             debug::error(FUNCTION, "failed to verify");
@@ -165,8 +38,13 @@ int main(int argc, char **argv)
 
     runtime::timer timer;
     timer.Start();
-    FLKey key;
-    key.SetSecret(hashGenerate.GetBytes());
+    LLC::FLKey key;
+
+    /* Get the secret from new key. */
+    std::vector<uint8_t> vBytes = hashGenerate.GetBytes();
+    LLC::CSecret vchSecret(vBytes.begin(), vBytes.end());
+
+    key.SetSecret(vchSecret, true);
     uint64_t nElapsed = timer.ElapsedMicroseconds();
     debug::log(0, FUNCTION, "Generated in ", nElapsed, " microseconds");
     timer.Reset();
@@ -185,7 +63,7 @@ int main(int argc, char **argv)
 
     debug::log(0, "PubSize ", vchPubKey.size());
 
-    FLKey key2;
+    LLC::FLKey key2;
     key2.SetPubKey(vchPubKey);
     if(!key2.Verify(vchMessage, vchSignature))
         debug::error(FUNCTION, "failed to verify");
