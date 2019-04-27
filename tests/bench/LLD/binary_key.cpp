25ba01b59057295f66cd927ffaaf6f1383f6071d
VidereLicet <colin@nexus.io>
2019-04-27 01:27:09 -0700

    Add xxHash into Template LRU and Key LRU, apply benchmark tests for both LRU for analysis on speed improvements

--- tests/bench/LLD/binary_key.cpp
+++ tests/bench/LLD/binary_key.cpp
@@ -2,7 +2,7 @@
 
 #include <LLC/include/random.h>
 
-#include <LLD/cache/binary_lru.h>
+#include <LLD/cache/binary_key.h>
 
 #include <LLD/include/version.h>
 
@@ -11,12 +11,12 @@
 #include <unit/catch2/catch.hpp>
 
 
-TEST_CASE( "Binary LRU Benchmarks", "[LLD]" )
+TEST_CASE( "Binary Key LRU Benchmarks", "[LLD]" )
 {
-    debug::log(0, "===== Begin Binary LRU Benchmarks =====");
+    debug::log(0, "===== Begin Binary Key LRU Benchmarks =====");
 
     //benchmarks
-    LLD::BinaryLRU* cache = new LLD::BinaryLRU();
+    LLD::KeyLRU* cache = new LLD::KeyLRU();
     uint256_t hash = LLC::GetRand256();
     {
         runtime::timer timer;
@@ -27,10 +27,7 @@ TEST_CASE( "Binary LRU Benchmarks", "[LLD]" )
             DataStream ssKey(SER_LLD, LLD::DATABASE_VERSION);
             ssKey << std::make_pair(std::string("data"), hash + i);
 
-            DataStream ssData(SER_LLD, LLD::DATABASE_VERSION);
-            ssData << uint1024_t(4934943);
-
-            cache->Put(ssKey.Bytes(), ssData.Bytes());
+            cache->Put(ssKey.Bytes());
         }
 
         uint64_t nTime = timer.ElapsedMicroseconds();
@@ -47,15 +44,13 @@ TEST_CASE( "Binary LRU Benchmarks", "[LLD]" )
             DataStream ssKey(SER_LLD, LLD::DATABASE_VERSION);
             ssKey << std::make_pair(std::string("data"), hash + i);
 
-            std::vector<uint8_t> vBytes;
-
-            cache->Get(ssKey.Bytes(), vBytes);
+            cache->Has(ssKey.Bytes());
         }
 
         uint64_t nTime = timer.ElapsedMicroseconds();
-        debug::log(0, ANSI_COLOR_BRIGHT_CYAN, "Get::", ANSI_COLOR_RESET, 1000000.0 / nTime, " million records / second");
+        debug::log(0, ANSI_COLOR_BRIGHT_CYAN, "Has::", ANSI_COLOR_RESET, 1000000.0 / nTime, " million records / second");
     }
 
 
-    debug::log(0, "===== End Binary LRU Benchmarks =====\n");
+    debug::log(0, "===== End Binary Key LRU Benchmarks =====\n");
 }
---
+++ tests/bench/LLD/template_lru.cpp
@@ -0,0 +1,52 @@
+#include <Util/include/runtime.h>
+
+#include <LLC/include/random.h>
+
+#include <LLD/cache/template_lru.h>
+
+#include <LLD/include/version.h>
+
+#include <Util/templates/datastream.h>
+
+#include <unit/catch2/catch.hpp>
+
+
+TEST_CASE( "Template LRU Benchmarks", "[LLD]" )
+{
+    debug::log(0, "===== Begin Template LRU Benchmarks =====");
+
+    //benchmarks
+    LLD::TemplateLRU<uint256_t, std::string>* cache = new LLD::TemplateLRU<uint256_t, std::string>(1024);
+
+    uint256_t hash = LLC::GetRand256();
+    {
+        runtime::timer timer;
+        timer.Start();
+
+        for(int i = 0; i < 1000000; i++)
+        {
+            cache->Put(hash + i, std::string("data"));
+        }
+
+        uint64_t nTime = timer.ElapsedMicroseconds();
+        debug::log(0, ANSI_COLOR_BRIGHT_CYAN, "Put::", ANSI_COLOR_RESET, 1000000.0 / nTime, " million records / second");
+    }
+
+
+    {
+        runtime::timer timer;
+        timer.Start();
+
+        for(int i = 0; i < 1000000; i++)
+        {
+            std::string str;
+            cache->Get(hash + i, str);
+        }
+
+        uint64_t nTime = timer.ElapsedMicroseconds();
+        debug::log(0, ANSI_COLOR_BRIGHT_CYAN, "Has::", ANSI_COLOR_RESET, 1000000.0 / nTime, " million records / second");
+    }
+
+
+    debug::log(0, "===== End Template LRU Benchmarks =====\n");
+}
