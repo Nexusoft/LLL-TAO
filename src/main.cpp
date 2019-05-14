429f9c379a418b9fb0b26832cb7c8ba0559b394f
Jack <jackdmcgowen@gmail.com>
2019-05-14 14:49:29 -0700

    updated main with default create directory flags so default data dir will be created.

--- src/main.cpp
+++ src/main.cpp
@@ -92,10 +92,10 @@ int main(int argc, char** argv)
 
 
     /* Create directories if they don't exist yet. */
-    if(!filesystem::exists(config::GetDataDir(false)) &&
-        filesystem::create_directory(config::GetDataDir(false)))
+    if(!filesystem::exists(config::GetDataDir()) &&
+        filesystem::create_directory(config::GetDataDir()))
     {
-        debug::log(0, FUNCTION, "Generated Path ", config::GetDataDir(false));
+        debug::log(0, FUNCTION, "Generated Path ", config::GetDataDir());
     }
 
 
