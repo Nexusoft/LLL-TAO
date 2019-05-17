1f49f1928d7402e3abb062cc4d11513ec3fbdae7
scottsimon36 <33037322+scottsimon36@users.noreply.github.com>
2019-05-17 00:12:22 -0500

    Create stake.cpp for PoS settings and calculations

---
+++ src/TAO/Ledger/include/stake.h
@@ -0,0 +1,223 @@
+/*__________________________________________________________________________________________
+
+			(c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+
+			(c) Copyright The Nexus Developers 2014 - 2019
+
+			Distributed under the MIT software license, see the accompanying
+			file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+			"ad vocem populi" - To the Voice of the People
+
+____________________________________________________________________________________________*/
+
+#pragma once
+#ifndef NEXUS_TAO_LEDGER_INCLUDE_STAKE_H
+#define NEXUS_TAO_LEDGER_INCLUDE_STAKE_H
+
+#include <TAO/Ledger/types/sigchain.h> 
+#include <TAO/Ledger/types/state.h> 
+#include <TAO/Ledger/types/transaction.h> 
+
+#include <Util/include/memory.h>
+
+
+/** 
+ *  The functions defined here provide a single source for settings and calculations related to Nexus Proof of Stake.
+ * 
+ *  Settings-related functions replace direct references to defined constants in the code, allowing for 
+ *  activation-triggered changes to be coded in one place rather than in multiple places throughout the code. 
+ *  Any other changes, such as alternative forms of definition, would also be encapsulated within
+ *  these functions and not require any change elsewhere in the code.
+ *
+ *  Similarly, functions that perform calculations isolate the definition of these calculations into a
+ *  single location, supporting use in multiple places while only having the calculation itself
+ *  coded once. 
+ *
+ **/
+
+/* Global TAO namespace. */
+namespace TAO
+{
+
+    /* Ledger Layer namespace. */
+    namespace Ledger
+    {
+
+        /** FindLastStake
+         *
+         *  Retrieves the most recent stake transaction for a user account.
+         *
+         *  @param[in] user - the user account signature chain
+         *  @param[out] tx - the most recent stake transaction
+         *
+         *  @return true if the last stake transaction was successfully retrieved
+         *
+         **/
+        bool FindLastStake(const memory::encrypted_ptr<TAO::Ledger::SignatureChain>& user, TAO::Ledger::Transaction& tx);
+
+
+        /** MaxBlockAge
+         *
+         *  Retrieve the setting for maximum block age (time since last stake block mined)
+         *  before trust decay begins.
+         *
+         *  @return the current system setting for maximum block age
+         *
+         **/
+        uint64_t MaxBlockAge();
+
+
+        /** MaxTrustScore
+         *
+         *  Retrieve the setting for maximum trust score value allowed. 
+         *
+         *  @return the current system setting for maximum trust score
+         *
+         **/
+        uint64_t MaxTrustScore();
+
+
+        /** MinCoinAge
+         *
+         *  Retrieve the setting for minimum coin age required to begin staking Genesis.
+         *
+         *  @return the current system setting for minimum coin age
+         *
+         **/
+        uint64_t MinCoinAge();
+
+
+        /** MinStakeInterval
+         *
+         *  Retrieve the minimum number of blocks required between an account's stake transactions.
+         *
+         *  @return the current system setting for minimum stake interval
+         *
+         **/
+        uint32_t MinStakeInterval();
+
+
+        /** TrustWeightBase
+         *
+         *  Retrieve the base time value for calculating trust weight.
+         *
+         *  @return the current system setting for trust weight base
+         *
+         **/
+        uint64_t TrustWeightBase();
+
+
+        /** TrustScore 
+         *
+         *  Calculate new trust score from parameters.
+         *
+         *  @param[in] nTrustPrev - previous trust score of trust account
+         *  @param[in] nStakePrev - previous stake balance,  
+         *  @param[in] nStakeNew - new stake balance after any pending change applied
+         *  @param[in] nBlockAge - current block age (time since last stake block for trust account)
+         *
+         *  @return new value for trust score
+         *
+         **/
+        uint64_t TrustScore(const uint64_t nTrustPrev, const uint64_t nStakePrev, const uint64_t nStakeNew, const uint64_t nBlockAge);
+
+
+        /** BlockWeight 
+         *
+         *  Calculate the proof of stake block weight for a given block age.
+         *
+         *  @param[in] nBlockAge 
+         *
+         *  @return value for block weight
+         *
+         **/
+        double BlockWeight(const uint64_t nBlockAge);
+
+
+        /** GenesisWeight 
+         *
+         *  Calculate the equivalent proof of stake trust weight for staking Genesis with a given coin age.
+         *
+         *  @param[in] nCoinAge 
+         *
+         *  @return value for trust weight
+         *
+         **/
+        double GenesisWeight(const uint64_t nCoinAge);
+
+
+        /** TrustWeight 
+         *
+         *  Calculate the proof of stake trust weight for a given trust score.
+         *
+         *  @param[in] nTrust - Trust score 
+         *
+         *  @return value for trust weight
+         *
+         **/
+        double TrustWeight(const uint64_t nTrust);
+
+
+        /** CurrentThreshold 
+         *
+         *  Calculate the current threshold value for Proof of Stake.
+         *  This value must exceed required threshold for staking to proceed.
+         *
+         *  @param[in] nBlockTime - Amount of time since last block generated on the network
+         *  @param[in] nNonce - Nonce value for stake iteration
+         *
+         *  @return value for current threshold
+         *
+         **/
+        double CurrentThreshold(const uint64_t nBlockTime, const uint64_t nNonce);
+
+
+        /** RequiredThreshold 
+         *
+         *  Calculate the minimum Required Energy Efficiency Threshold.
+         *  Can only mine Proof of Stake when current threshold exceeds this value.
+         *
+         *  @param[in] nTrustWeight - Current trust weight
+         *  @param[in] nBlockWeight - Current block weight
+         *  @param[in] nStake - Current stake balance
+         *
+         *  @return value for minimum required threshold
+         *
+         **/
+        double RequiredThreshold(const double nTrustWeight, const double nBlockWeight, const uint64_t nStake);
+
+
+        /** StakeRate 
+         *
+         *  Calculate the stake rate corresponding to a given trust score.
+         *
+         *  Returned value is absolute rate. To display a percent, multiply by 100. 
+         *
+         *  @param[in] nTrust - Current trust score (ignored when fGenesis = true)
+         *  @param[in] isGenesis - Set true if staking for Genesis transaction
+         *
+         *  @return value for stake rate
+         *
+         **/
+        double StakeRate(const uint64_t nTrust, const bool isGenesis = false);
+
+
+        /** CoinstakeReward 
+         *
+         *  Calculate the coinstake reward for a given stake.
+         *
+         *  @param[in] nStake - Stake balance on which reward is to be paid
+         *  @param[in] nStakeTime - Amount of time reward is for
+         *  @param[in] nTrust - Current trust score (ignored when fGenesis = true)
+         *  @param[in] isGenesis - Set true if staking for Genesis transaction
+         *
+         *  @return amount of coinstake reward
+         *
+         **/
+        uint64_t CoinstakeReward(const uint64_t nStake, const uint64_t nStakeTime, const uint64_t nTrust, const bool isGenesis = false);
+
+    }
+}
+
+#endif
---
+++ src/TAO/Ledger/stake.cpp
@@ -0,0 +1,249 @@
+/*__________________________________________________________________________________________
+
+			(c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+
+			(c) Copyright The Nexus Developers 2014 - 2019
+
+			Distributed under the MIT software license, see the accompanying
+			file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+			"ad vocem populi" - To the Voice of the People
+
+____________________________________________________________________________________________*/
+
+#include <TAO/Ledger/include/stake.h>
+
+#include <LLC/types/uint1024.h>
+
+#include <LLD/include/global.h>
+
+#include <TAO/Ledger/include/constants.h>
+#include <TAO/Ledger/include/timelocks.h>
+
+#include <TAO/Operation/include/enum.h>
+
+#include <TAO/Register/include/unpack.h>
+
+#include <Util/include/args.h>
+#include <Util/include/debug.h>
+#include <Util/include/runtime.h>
+
+
+/* Global TAO namespace. */
+namespace TAO
+{
+
+    /* Ledger Layer namespace. */
+    namespace Ledger
+    {
+
+        /* Constants for use in staking calculations (move to TAO/Ledger/include/constants.h ?) */
+        const double LOG3 = log(3); 
+        const double LOG10 = log(10); 
+
+
+        /** Retrieves the most recent stake transaction for a user account. */
+        bool FindLastStake(const memory::encrypted_ptr<TAO::Ledger::SignatureChain>& user, TAO::Ledger::Transaction& tx)
+        {
+            uint512_t hashLast = 0;
+
+            /* Get the most recent tx hash for the user account. */
+            if (!LLD::legDB->ReadLast(user->Genesis(), hashLast))
+                return false;
+
+            /* Loop until find stake transaction or reach first transaction on user acount (hashLast == 0). */
+            while (hashLast != 0)
+            {
+                /* Get the transaction for the current hashLast. */
+                TAO::Ledger::Transaction txCheck;
+                if(!LLD::legDB->ReadTx(hashLast, txCheck))
+                    return false;
+
+                /* Test whether the transaction contains a staking operation */
+                if (TAO::Register::Unpack(txCheck, TAO::Operation::OP::TRUST) || TAO::Register::Unpack(txCheck, TAO::Operation::OP::GENESIS))
+                {
+                    /* Found last stake transaction. */
+                    tx = txCheck;
+                    return true;
+                }
+
+                /* Stake tx not found, yet, iterate to next previous user tx */
+                hashLast = txCheck.hashPrevTx;
+            }
+
+            return false;
+        }
+
+
+        /* Retrieve the setting for maximum block age (time since last stake before trust decay begins. */
+        uint64_t MaxBlockAge()
+        {
+            return (uint64_t)(config::fTestNet ? TAO::Ledger::TRUST_KEY_TIMESPAN_TESTNET : TAO::Ledger::TRUST_KEY_TIMESPAN);
+        }
+
+
+        /* Retrieve the setting for maximum trust score value allowed. */
+        uint64_t MaxTrustScore()
+        {
+            return (uint64_t)(config::fTestNet ? TAO::Ledger::TRUST_SCORE_MAX_TESTNET : TAO::Ledger::TRUST_SCORE_MAX);
+        }
+
+
+        /* Retrieve the setting for minimum coin age required to begin staking Genesis.*/
+        uint64_t MinCoinAge()
+        {
+            return (uint64_t)(config::fTestNet ? TAO::Ledger::MINIMUM_GENESIS_COIN_AGE_TESTNET : TAO::Ledger::MINIMUM_GENESIS_COIN_AGE);
+        }
+
+
+        /* Retrieve the minimum number of blocks required between an account's stake transactions.*/
+        uint32_t MinStakeInterval()
+        {
+            if (config::fTestNet)
+                return TAO::Ledger::TESTNET_MINIMUM_INTERVAL;
+
+            else if (TAO::Ledger::NETWORK_BLOCK_CURRENT_VERSION < 7)
+                return TAO::Ledger::MAINNET_MINIMUM_INTERVAL_LEGACY;
+
+            else if (runtime::timestamp() > TAO::Ledger::NETWORK_VERSION_TIMELOCK[5])  //timelock activation for Tritium interval
+                return TAO::Ledger::MAINNET_MINIMUM_INTERVAL;
+
+            else
+                return TAO::Ledger::MAINNET_MINIMUM_INTERVAL_LEGACY;
+        }
+
+
+        /* Retrieve the base trust score setting for performing trust weight calculations. */
+        uint64_t TrustWeightBase()
+        {
+            return (uint64_t)(config::fTestNet ? TAO::Ledger::TRUST_WEIGHT_BASE_TESTNET : TAO::Ledger::TRUST_WEIGHT_BASE);
+        }
+
+
+        /* Calculate new trust score from parameters. */
+        uint64_t TrustScore(const uint64_t nTrustPrev, const uint64_t nStakePrev, const uint64_t nStakeNew, const uint64_t nBlockAge)
+        {
+            uint64_t nTrust;
+            uint64_t nTrustMax = MaxTrustScore();
+
+            /* Block age less than maximum awards trust score increase equal to the current block age. */
+            if (nBlockAge <= MaxBlockAge())
+            {
+                nTrust = std::min((nTrustPrev + nBlockAge), nTrustMax);
+            }
+            else
+            {
+                /* Block age more than maximum allowed is penalized 3 times the time it has exceeded the maximum. */
+
+                /* Calculate the penalty for score (3x the time). */
+                uint64_t nPenalty = (nBlockAge - MaxBlockAge()) * (uint64_t)3;
+
+                /* Trust back to zero if penalty more than previous score. */
+                nTrust = std::max((nTrustPrev - nPenalty), (uint64_t)0);
+            }
+
+            if (nTrust > 0 && nStakeNew < nStakePrev)
+            {
+                /* Removing stake balance incurs trust penalty */
+                uint64_t nBalanceRemoved = nStakePrev - nStakeNew;
+
+                /* Trust reduced by the percent of balance removed */
+                nTrust = nTrust - (nTrust * nBalanceRemoved) / nStakePrev;
+            }
+
+            /* Double check that the trust score cannot exceed the maximum */
+            if (nTrust > nTrustMax)
+                nTrust = nTrustMax;
+
+            return nTrust;
+        }
+
+
+        /* Calculate the proof of stake block weight for a given block age. */
+        double BlockWeight(const uint64_t nBlockAge)
+        {
+
+            /* Block Weight reaches maximum of 10.0 when Block Age equals the max block age */
+            double nBlockAgeRatio = (double)nBlockAge / (double)MaxBlockAge();
+
+            return std::min(10.0, (9.0 * log((2.0 * nBlockAgeRatio) + 1.0) / LOG3) + 1.0);
+        }
+
+
+        /* Calculate the equivalent proof of stake trust weight for staking Genesis with a given coin age. */
+        double GenesisWeight(const uint64_t nCoinAge)
+        {
+            /* Trust Weight For Genesis is based on Coin Age. Genesis trust weight is less than normal trust weight,
+             * reaching a maximum of 10.0 after average Coin Age reaches trust weight base.
+             */
+            double nGenesisTrustRatio = (double)nCoinAge / (double)TrustWeightBase();
+
+            return std::min(10.0, (9.0 * log((2.0 * nGenesisTrustRatio) + 1.0) / LOG3) + 1.0);
+        }
+
+
+        /* Calculate the proof of stake trust weight for a given trust score. */
+        double TrustWeight(const uint64_t nTrust)
+        {
+            /* Trust Weight base is time for 50% score. Weight continues to grow with Trust Score until it reaches max of 90.0
+             * This formula will reach 45.0 (50%) after accumulating 84 days worth of Trust Score (Mainnet base), 
+             * while requiring close to a year to reach maximum.
+             */
+            double nTrustWeightRatio = (double)nTrust / (double)TrustWeightBase();
+
+            return std::min(90.0, (44.0 * log((2.0 * nTrustWeightRatio) + 1.0) / LOG3) + 1.0);
+        }
+
+
+        /* Calculate the current threshold value for Proof of Stake. */
+        double CurrentThreshold(const uint64_t nBlockTime, const uint64_t nNonce)
+        {
+            return (nBlockTime * 100.0) / nNonce;
+        }
+
+
+        /* Calculate the minimum Required Energy Efficiency Threshold. */
+        double RequiredThreshold(double nTrustWeight, double nBlockWeight, uint64_t nStake)
+        {
+            /*  Staking weights (trust and block) reduce the required threshold by reducing the numerator of this calculation.
+             *  Weight from staking balance reduces the required threshold by increasing the denominator.
+             */
+            return ((108.0 - nTrustWeight - nBlockWeight) * TAO::Ledger::MAX_STAKE_WEIGHT) / nStake;
+        }
+
+
+        /* Calculate the stake rate corresponding to a given trust score. */
+        double StakeRate(const uint64_t nTrust, const bool isGenesis)
+        {
+            /* Stake rate fixed at 0.005 (0.5%) when staking Genesis */
+            if (isGenesis)
+                return 0.005;
+
+            /* Stake rate starts at 0.005 (0.5%) and grows to 0.03 (3%) when trust score reaches maximum */
+            double nTrustScoreRatio = (double)nTrust / (double)MaxTrustScore();
+
+            return std::min(0.03, (0.025 * log((9.0 * nTrustScoreRatio) + 1.0) / LOG10) + 0.005);
+        }
+
+
+        /* Calculate the coinstake reward for a given stake. */
+        uint64_t CoinstakeReward(const uint64_t nStake, const uint64_t nStakeTime, const uint64_t nTrust, const bool isGenesis)
+        {
+
+            double nStakeRate = StakeRate(nTrust, isGenesis);
+
+            /* Reward rate for time period is annual rate * (time period / annual time) or nStakeRate * (nStakeTime / MaxTrustScore)
+             * Then, overall nStakeReward = nStake * reward rate
+             *
+             * Thus, the appropriate way to write this (for clarity) would be: nStakeReward = nStake * nStakeRate * (nStakeTime / MaxTrustScore)
+             * However, with integer arithmetic (nStakeTime / MaxTrustScore) would evaluate to 0 or 1, etc. and the overall nStakeReward would be erroneous
+             *
+             * Therefore, we apply parentheses around the full multiplication portion before applying the division to get appropriate reward. 
+             */
+            uint64_t nStakeReward = (nStake * nStakeRate * nStakeTime) / MaxTrustScore();
+
+            return nStakeReward;
+        }
+
+    }
+}
