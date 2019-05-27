0edc503b814784b699aa946427526c4032c5c20a
VidereLicet <colin@nexus.io>
2019-05-27 16:57:01 -0700

    Add register operation benchmarks

---
+++ tests/bench/TAO/Operation/register.cpp
@@ -0,0 +1,57 @@
+#include <LLC/include/random.h>
+
+#include <LLD/include/global.h>
+
+#include <TAO/Operation/include/execute.h>
+#include <TAO/Operation/include/enum.h>
+
+#include <TAO/Register/include/enum.h>
+
+#include <TAO/Ledger/types/transaction.h>
+
+#include <unit/catch2/catch.hpp>
+
+
+
+TEST_CASE( "OP::REGISTER  Benchmarks", "[operation]" )
+{
+    using namespace TAO::Operation;
+
+    debug::log(0, "===== Begin OP::REGISTER Benchmarks =====");
+
+    {
+        std::vector<uint8_t> vData(32, 0xff);
+
+        std::vector<TAO::Ledger::Transaction> vTx;
+        for(int i = 0; i < 10000; ++i)
+        {
+            TAO::Ledger::Transaction tx;
+            tx.hashGenesis = LLC::GetRand256();
+            tx << uint8_t(OP::REGISTER) << LLC::GetRand256() << uint8_t(TAO::Register::REGISTER::READONLY) << vData;
+
+            REQUIRE(TAO::Operation::Execute(tx, TAO::Register::FLAGS::PRESTATE | TAO::Register::FLAGS::POSTSTATE));
+
+            vTx.push_back(tx);
+        }
+
+
+        //Benchmark of 10,000 executions
+        runtime::timer bench;
+        bench.Reset();
+        {
+            LLD::TxnBegin();
+            for(int i = 0; i < vTx.size(); ++i)
+            {
+                REQUIRE(TAO::Operation::Execute(vTx[i], TAO::Register::FLAGS::MEMPOOL));
+            }
+            LLD::TxnCommit();
+        }
+
+        //time output
+        uint64_t nTime = bench.ElapsedMilliseconds();
+        debug::log(0, ANSI_COLOR_BRIGHT_CYAN, "OP::REGISTER", ANSI_COLOR_RESET, " Processed ", 10000000.0 / nTime, " ops / second");
+    }
+
+    debug::log(0, "===== End OP::REGISTER Benchmarks =====\n");
+
+}
--- tests/bench/main.cpp
+++ tests/bench/main.cpp
@@ -14,13 +14,36 @@ ________________________________________________________________________________
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <unit/catch2/catch.hpp>
 
+#include <LLD/include/global.h>
+
+#include <Util/include/filesystem.h>
 #include <Util/include/args.h>
 
 TEST_CASE("Arguments Tests", "[args]")
 {
-    config::fTestNet.load() = true;
+    config::fTestNet = true;
     config::mapArgs["-testnet"] = "92349234";
 
     REQUIRE(config::fTestNet.load() == true);
     REQUIRE(config::GetArg("-testnet", 0) == 92349234);
+
+    //get the data directory
+    std::string strPath = config::GetDataDir();
+
+    //test the filesystem remove and also clear from previous unit tests
+    if(filesystem::exists(strPath))
+    {
+        REQUIRE(filesystem::remove_directories(strPath));
+        REQUIRE(!filesystem::exists(strPath));
+    }
+
+    /* Create the database instances. */
+    LLD::regDB = new LLD::RegisterDB(LLD::FLAGS::CREATE | LLD::FLAGS::FORCE);
+    LLD::locDB = new LLD::LocalDB(LLD::FLAGS::CREATE | LLD::FLAGS::FORCE);
+    LLD::legDB = new LLD::LedgerDB(LLD::FLAGS::CREATE | LLD::FLAGS::FORCE);
+
+
+    /* Initialize the Legacy Database. */
+    LLD::trustDB  = new LLD::TrustDB(LLD::FLAGS::CREATE | LLD::FLAGS::FORCE);
+    LLD::legacyDB = new LLD::LegacyDB(LLD::FLAGS::CREATE | LLD::FLAGS::FORCE);
 }
