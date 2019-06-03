2b10f4581da3f467323f4c986d9e432bab03f734
Paul Screen <paul@nexus.io>
2019-06-03 16:35:00 +1000

    Added Supply API to smoke test

--- tests/smoke/apitest.sh
+++ tests/smoke/apitest.sh
@@ -130,3 +130,26 @@
 #Tokenize an asset
 ./nexus assets/tokenize/asset name=myasset token_name=abc pin=1234 
 
+
+## SUPPLY ##
+#Create a supply item with simple data
+./nexus supply/create/item name=item1 data=123456789 pin=1234
+
+#Retrieve an item by name
+./nexus supply/get/item/item1
+
+#Retrieve an item by address
+./nexus supply/get/item/ee0ec5255644f730a9574b6813b6de2e7a892253c039cac77cd9af0ffcac10b8
+
+#Update the item data 
+./nexus supply/update/item/item1 data=987654321 pin=1234
+
+#View an item history
+./nexus supply/list/item/history/item1
+
+#Transfer an item to another user
+./nexus supply/transfer/item/item1 username=jack pin=1234
+
+#Claim an item transferred to you
+./nexus supply/claim/item txid=430a91929c003f577f8d3a8f6388287d5353df2c3aef4149d33b9de0564808abd12d2a14c4c056968e73bc1276f93bfbfff7d6e4eabbae801b9d76c5a1f65015 pin=1234
+
