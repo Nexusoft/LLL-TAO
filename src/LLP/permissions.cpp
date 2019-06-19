ccc387c9cd8af6025d2be64bcea753246e669bd4
Jack <jackdmcgowen@gmail.com>
2019-06-19 14:41:24 -0700

    fixed bug with connecting to node via IPv6 on local network. Got rid of unused WildcardMatch function

--- src/LLP/permissions.cpp
+++ src/LLP/permissions.cpp
@@ -19,8 +19,12 @@ ________________________________________________________________________________
 
 
 /*  IP Filtering Definitions. IP's are Filtered By Ports. */
-bool CheckPermissions(const std::string &strAddress, uint32_t nPort)
+bool CheckPermissions(const std::string &strAddress, uint16_t nPort)
 {
+    /* If no llpallowip whitelist has been defined for this port then we assume they are allowed */
+    if(config::mapIPFilters[nPort].size() == 0)
+        return true;
+
     /* Bypass localhost addresses first. */
     if(strAddress == "127.0.0.1" || strAddress == "::1")
         return true;
@@ -31,14 +35,8 @@ bool CheckPermissions(const std::string &strAddress, uint32_t nPort)
         return debug::error("Address size not at least 4 bytes.");
 
     /* Check against the llpallowip list from config / commandline parameters. */
-
-    /* If no llpallowip whitelist has been defined for this port then we assume they are allowed */
-    if(config::mapIPFilters[nPort].size() == 0)
-        return true;
-
     for(const auto& strIPFilter : config::mapIPFilters[nPort])
     {
-
         /* Split the components of the IP so that we can check for wildcard ranges. */
         std::vector<std::string> vCheck = Split(strIPFilter, '.');
 
@@ -49,47 +47,15 @@ bool CheckPermissions(const std::string &strAddress, uint32_t nPort)
         /* Check the components of IP address. */
         bool fIPMatches = true;
         for(uint8_t nByte = 0; nByte < 4; ++nByte)
+        {
             if(vCheck[nByte] != "*" && vCheck[nByte] != vAddress[nByte])
                 fIPMatches = false;
+        }
 
         /* if the IP matches then the address being checked is on the whitelist */
         if(fIPMatches)
             return true;
-
     }
 
     return false;
-}
-
-
-/* Performs a wildcard match string search given a search string and a mask. */
-bool WildcardMatch(const char* psz, const char* mask)
-{
-    while(true)
-    {
-        switch (*mask)
-        {
-        case '\0':
-            return (*psz == '\0');
-        case '*':
-            return WildcardMatch(psz, mask+1) || (*psz && WildcardMatch(psz+1, mask));
-        case '?':
-            if(*psz == '\0')
-                return false;
-            break;
-        default:
-            if(*psz != *mask)
-                return false;
-            break;
-        }
-        ++psz;
-        ++mask;
-    }
-}
-
-
-/* Performs a wildcard match string search given a search string and a mask. */
-bool WildcardMatch(const std::string& str, const std::string& mask)
-{
-    return WildcardMatch(str.c_str(), mask.c_str());
 }
