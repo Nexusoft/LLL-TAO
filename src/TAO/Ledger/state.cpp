f3ed6115f70be8c96c11e47d660d59cfff3aff1e
VidereLicet <colin@nexus.io>
2019-07-02 14:56:53 -0700

    Use 128 bit for weights, update ledger weighting rules

--- src/TAO/Ledger/state.cpp
+++ src/TAO/Ledger/state.cpp
@@ -364,7 +364,6 @@ namespace TAO
             /* Write the transactions. */
             for(const auto& proof : vtx)
             {
-                /* Only work on tritium transactions for now. */
                 if(proof.first == TYPE::TRITIUM_TX)
                 {
                     /* Get the transaction hash. */
@@ -419,18 +418,27 @@ namespace TAO
             if(nVersion >= 7 && !IsPrivate())
             {
                 /* Set the chain trust. */
-                uint8_t nTotal = 0;
+                uint8_t nEquals  = 0;
+                uint8_t nGreater = 0;
 
                 /* Check to best state. */
                 for(uint32_t n = 0; n < 3; ++n)
                 {
                     /* Check each weight. */
-                    if(nChannelWeight[nChannel] >= ChainState::stateBest.load().nChannelWeight[n])
-                        ++nTotal;
+                    if(nChannelWeight[nChannel] == ChainState::stateBest.load().nChannelWeight[n])
+                        ++nEquals;
+
+                    /* Check each weight. */
+                    if(nChannelWeight[nChannel] > ChainState::stateBest.load().nChannelWeight[n])
+                        ++nGreater;
                 }
 
-                /* Check for best chain. */
-                if(nTotal == 3 && !SetBest())
+                /* Handle two channels having greater weight. */
+                if(nGreater > 1 && !SetBest())
+                    return debug::error(FUNCTION, "failed to set best chain");
+
+                /* Handle single channel having higher weight. */
+                if(nEquals == 2 && nGreater == 1 && !SetBest())
                     return debug::error(FUNCTION, "failed to set best chain");
             }
             else if(nChainTrust > ChainState::nBestChainTrust.load() && !SetBest())
