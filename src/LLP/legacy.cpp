36676604643e4c91b5f7a1106933fa07c639d4dc
VidereLicet <colin@nexus.io>
2019-07-10 21:54:42 -0700

    Set fast sync timeouts to 15 seconds

--- src/LLP/legacy.cpp
+++ src/LLP/legacy.cpp
@@ -223,8 +223,8 @@ namespace LLP
             /* Unreliabilitiy re-requesting (max time since getblocks) */
             if(TAO::Ledger::ChainState::Synchronizing()
             && addrFastSync == GetAddress()
-            && nLastTimeReceived.load() + 30 < nTimestamp
-            && nLastGetBlocks.load() + 30 < nTimestamp)
+            && nLastTimeReceived.load() + 15 < nTimestamp
+            && nLastGetBlocks.load() + 15 < nTimestamp)
             {
                 debug::log(0, NODE, "fast sync event timeout");
 
