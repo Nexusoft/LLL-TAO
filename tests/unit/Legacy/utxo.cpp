159db6f2bcb6f0dd95609c1bdfa9f470ff59c661
VidereLicet <colin@nexus.io>
2019-07-12 19:20:47 -0700

    Add UTXO unit tests

---
+++ tests/unit/Legacy/utxo.cpp
@@ -0,0 +1,20 @@
+/*__________________________________________________________________________________________
+
+            (c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
+
+            (c) Copyright The Nexus Developers 2014 - 2019
+
+            Distributed under the MIT software license, see the accompanying
+            file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+            "ad vocem populi" - To the Voice of the People
+
+____________________________________________________________________________________________*/
+
+#include <Legacy/types/transaction.h>
+#include <unit/catch2/catch.hpp>
+
+TEST_CASE("UTXO Unit Tests", "[UTXO]")
+{
+    Legacy::Transaction tx;
+}
