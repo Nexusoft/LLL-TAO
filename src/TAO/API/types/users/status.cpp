4d199ddb63936c2d9e847cd980abea4912f71719
Paul Screen <paul@nexus.io>
2019-08-29 20:17:26 +1000

    Users/get/status - added genesis, transactions, & notifications

--- src/TAO/API/types/users/status.cpp
+++ src/TAO/API/types/users/status.cpp
@@ -47,8 +47,39 @@ namespace TAO
             /* Get the sigchain from map of users. */
             memory::encrypted_ptr<TAO::Ledger::SignatureChain>& user = mapSessions[0];
 
+            uint256_t hashGenesis = user->Genesis();
             /* populate response */
             ret["username"] = user->UserName().c_str();
+            ret["genesis"] = hashGenesis.GetHex();
+
+            /* sig chain transaction count */
+            uint32_t nTransactions = 0;
+            
+            /* Read the last transaction for the sig chain */
+            uint512_t hashLast = 0;
+            LLD::Ledger->ReadLast(hashGenesis, hashLast, TAO::Ledger::FLAGS::MEMPOOL);
+
+            /* Loop until genesis to count the transactions */
+            while(hashLast != 0)
+            {
+                /* Get the transaction from disk. */
+                TAO::Ledger::Transaction tx;
+                if(!LLD::Ledger->ReadTx(hashLast, tx, TAO::Ledger::FLAGS::MEMPOOL))
+                    throw APIException(-108, "Failed to read transaction");
+
+                /* Set the next last. */
+                hashLast = tx.hashPrevTx;
+                ++nTransactions;
+            }
+
+            /* populate the transaction count */
+            ret["transactions"] = nTransactions;
+
+            /* Get the notifications so that we can return the notification count. */
+            std::vector<std::tuple<TAO::Operation::Contract, uint32_t, uint256_t>> vContracts;
+            GetOutstanding(hashGenesis, vContracts);
+            ret["notifications"] = vContracts.size();
+
 
             /* populate unlocked status */
             json::json jsonUnlocked;
