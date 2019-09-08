7c41b7f6892c4c57e17de1926d007836595b1461
VidereLicet <colin@nexus.io>
2019-09-07 22:21:03 -0700

    Check for complete synchronization if last index is also current best chain

--- src/LLP/tritium.cpp
+++ src/LLP/tritium.cpp
@@ -1215,7 +1215,7 @@ namespace LLP
                             if(nCurrentSession == TAO::Ledger::nSyncSession.load())
                             {
                                 /* Check for complete synchronization. */
-                                if(hashLast == TAO::Ledger::ChainState::hashBestChain.load())
+                                if(hashLast == TAO::Ledger::ChainState::hashBestChain.load() && hashLast == hashBestChain)
                                 {
                                     /* Set state to synchronized. */
                                     fSynchronized.store(true);
