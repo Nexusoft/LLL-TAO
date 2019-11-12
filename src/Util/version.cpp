d95df4e64c0b95b8f8bfa3ee1f4799c8fe7ec57a
VidereLicet <colin@nexus.io>
2019-11-12 09:43:47 -0700

    Move version to 4.0.3 full

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -66,5 +66,5 @@ namespace version
         const std::string BUILD_ARCH = "[x86]";
     #endif
 
-    const std::string CLIENT_VERSION_BUILD_STRING(CLIENT_VERSION_STRING + "-pre " + CLIENT_NAME  + " " + CLIENT_INTERFACE + " " + CLIENT_DATABASE + BUILD_ARCH);
+    const std::string CLIENT_VERSION_BUILD_STRING(CLIENT_VERSION_STRING + " " + CLIENT_NAME  + " " + CLIENT_INTERFACE + " " + CLIENT_DATABASE + BUILD_ARCH);
 }
