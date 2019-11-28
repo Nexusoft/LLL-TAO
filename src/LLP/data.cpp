e71b43ac4edca10751217650fc7e3e12fac3a059
VidereLicet <colin@nexus.io>
2019-11-28 16:24:52 -0700

    Adjust write timeout to one second, skip timeout check for available

--- src/LLP/data.cpp
+++ src/LLP/data.cpp
@@ -334,8 +334,7 @@ namespace LLP
 
                     /* Disconnect if pollin signaled with no data (This happens on Linux). */
                     if((POLLFDS.at(nIndex).revents & POLLIN)
-                    && CONNECTION->Available() == 0
-                    && CONNECTION->Timeout(3, Socket::READ))
+                    && CONNECTION->Available() == 0)
                     {
                         disconnect_remove_event(nIndex, DISCONNECT_POLL_EMPTY);
                         continue;
@@ -343,7 +342,7 @@ namespace LLP
 
                     /* Disconnect if buffer is full and remote host isn't reading at all. */
                     if(CONNECTION->Buffered()
-                    && CONNECTION->Timeout(3, Socket::WRITE))
+                    && CONNECTION->Timeout(1, Socket::WRITE))
                     {
                         disconnect_remove_event(nIndex, DISCONNECT_TIMEOUT_WRITE);
                         continue;
