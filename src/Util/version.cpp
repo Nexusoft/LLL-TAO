6a9cf6cbf33842d8746384a9fe3c16828b6ce473
VidereLicet <colin@nexus.io>
2019-12-16 14:26:03 -0700

    Tag for version 4.1.1

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -26,7 +26,7 @@ namespace version
 
 
     /* Patch version Z (x.y.Z | x > 0). */
-    const uint32_t CLIENT_PATCH = 0;
+    const uint32_t CLIENT_PATCH = 1;
 
 
     /* The version of the actual wallet client. */
