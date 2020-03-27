1223f5f2d9bb6e70856424ef58bbb9fd02ddc3e1
VidereLicet <colin@nexus.io>
2020-03-26 23:09:42 -0700

    Add sigchain merkle transaction relays for transactions connected to new blocks

--- src/TAO/Ledger/tritium.cpp
+++ src/TAO/Ledger/tritium.cpp
@@ -616,6 +616,7 @@ namespace TAO
             LLD::TxnBegin();
 
             /* Write the transactions. */
+            DataStream ssRelay(SER_NETWORK, LLP::PROTOCOL_VERSION);
             for(const auto& proof : vtx)
             {
                 /* Get the tritium transction. */
@@ -632,6 +633,10 @@ namespace TAO
                     /* Write to disk. */
                     if(!LLD::Ledger->WriteTx(hash, tx))
                         return debug::error(FUNCTION, "failed to write tx to disk");
+
+                    /* Handle sigchain related notifications. */
+                    if(LLP::TRITIUM_SERVER)
+                        ssRelay << uint8_t(LLP::TYPES::SIGCHAIN) << tx.hashGenesis << hash;
                 }
 
                 /* Get the legacy transaction. */
@@ -673,6 +678,16 @@ namespace TAO
             /* Check for best chain. */
             if(GetHash() == ChainState::hashBestChain.load())
             {
+                /* Relay the SIGCHAIN notifications. */
+                if(LLP::TRITIUM_SERVER)
+                {
+                    LLP::TRITIUM_SERVER->_Relay
+                    (
+                        uint8_t(LLP::ACTION::NOTIFY),
+                        ssRelay
+                    );
+                }
+
                 /* Do a quick mempool processing check for ORPHANS. */
                 runtime::timer timer;
                 timer.Reset();
