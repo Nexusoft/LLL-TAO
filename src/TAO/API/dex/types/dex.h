b4d1129020ae19f9bde6a53d33aaa4634a5d6f9c
Paul Screen <paul@nexus.io>
2020-11-26 16:56:47 +1100

    Moved location of dex utils

--- src/TAO/API/dex/types/dex.h
+++ src/TAO/API/dex/types/dex.h
@@ -25,41 +25,6 @@ namespace TAO
     namespace API
     {
 
-        /** DEXOrder
-         *
-         *  Encapsulates a DEX sell order, i.e. a conditional DEBIT / TRANSFER made to a wildcard address.
-         *  The order is always considered to be a sell order since the contract is always a debit/transfer for a specific token
-         *  or asset, with a condition to receive something in return.  However when viewing an orderbook, a DEXOrder may be a buy
-         *  or sell order, depending on which way around the market pair is being viewed. For example if the market pair is NXS/ABC
-         *  then a SELL order would be a debit of NXS in exchange for ABC, whereas a BUY order would be a debit of ABC in exchange
-         *  for NXS.  Therefore to compile a complete NXS/ABC orderbook you would need to read from the DEXDb all NXS/ABC orders 
-         *  for the sells and all ABC/NXS orders for the buys.   
-         *
-         **/
-        class DEXOrder
-        {
-        public:
-
-            /** The transaction ID containing this order **/
-            uint512_t hashTx;
-
-            /** The contract ID within the transaction for this order **/
-            uint32_t nContract;
-
-            /** The register address of the token or register being sold **/
-            TAO::Register::Address hashSell;
-
-            /** The quantity of tokens being sold.  If hashSell is a register instead of a token, this will be set to 1 **/
-            uint64_t nSellAmount;
-
-            /** The register address of the token being bought in exchange **/
-            TAO::Register::Address hashBuy;
-
-            /** The quantity of tokens being bought in exchange.  If hashBuy is a register instead of a token, this will be set to 1 **/
-            uint64_t nBuyAmount;
-
-        };
-
         /** DEX
          *
          *  DEX API Class.
