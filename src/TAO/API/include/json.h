926daf7e7497e8c6c40d0a9ab16f3b913791549a
VidereLicet <colin@nexus.io>
2021-06-24 13:13:30 -0700

    Adjust namespace formatting for our JSON functions, set their attributes as pure functions to allow lookups of non-volatile memory, but with guarentees they do not modify any global state

--- src/TAO/API/include/json.h
+++ src/TAO/API/include/json.h
@@ -16,162 +16,158 @@ ________________________________________________________________________________
 #include <LLC/types/uint1024.h>
 #include <Util/include/json.h>
 
-namespace Legacy { class Transaction; }
+namespace Legacy         { class Transaction; }
+
+/* Forward declarations. */
+namespace TAO::Operation { class Contract;    }
+namespace TAO::Register
+{
+    class Object;
+    class Address;
+}
+namespace TAO::Ledger
+{
+    class Transaction;
+    class BlockState;
+}
 
 /* Global TAO namespace. */
-namespace TAO
+namespace TAO::API
 {
-    namespace Operation { class Contract; }
-    namespace Register
-    {
-        class Object;
-        class Address;
-    }
-    namespace Ledger
-    {
-        class Transaction;
-        class BlockState;
-    }
-
-    /* API Layer namespace. */
-    namespace API
-    {
-        /** BlockToJSON
-         *
-         *  Converts the block to formatted JSON
-         *
-         *  @param[in] block The block to convert
-         *  @param[in] nVerbose determines the amount of transaction data to include in the response
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json BlockToJSON(const TAO::Ledger::BlockState& block, const uint32_t nVerbose);
+    /** BlockToJSON
+     *
+     *  Converts the block to formatted JSON
+     *
+     *  @param[in] block The block to convert
+     *  @param[in] nVerbose determines the amount of transaction data to include in the response
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    __attribute__((pure)) encoding::json BlockToJSON(const TAO::Ledger::BlockState& block, const uint32_t nVerbose);
 
 
-        /** TransactionToJSON
-         *
-         *  Converts the transaction to formatted JSON
-         *
-         *  @param[in] tx The transaction to convert to JSON
-         *  @param[in] block The block that the transaction exists in.  If null this will be loaded witin the method
-         *  @param[in] nVerbose determines the amount of transaction data to include in the response
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json TransactionToJSON(const TAO::Ledger::Transaction& tx, const TAO::Ledger::BlockState& block,
-                                         const uint32_t nVerbose = 0);
+    /** TransactionToJSON
+     *
+     *  Converts the transaction to formatted JSON
+     *
+     *  @param[in] tx The transaction to convert to JSON
+     *  @param[in] block The block that the transaction exists in.  If null this will be loaded witin the method
+     *  @param[in] nVerbose determines the amount of transaction data to include in the response
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    __attribute__((pure)) encoding::json TransactionToJSON(const TAO::Ledger::Transaction& tx,
+                                                           const TAO::Ledger::BlockState& block, const uint32_t nVerbose = 0);
 
 
-        /** TransactionToJSON
-         *
-         *  Converts the transaction to formatted JSON
-         *
-         *  @param[in] tx The transaction to convert to JSON
-         *  @param[in] block The block that the transaction exists in.  If null this will be loaded witin the method
-         *  @param[in] nVerbose determines the amount of transaction data to include in the response
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json TransactionToJSON(const Legacy::Transaction& tx, const TAO::Ledger::BlockState& block,
-                                         const uint32_t nVerbose = 0);
+    /** TransactionToJSON
+     *
+     *  Converts the transaction to formatted JSON
+     *
+     *  @param[in] tx The transaction to convert to JSON
+     *  @param[in] block The block that the transaction exists in.  If null this will be loaded witin the method
+     *  @param[in] nVerbose determines the amount of transaction data to include in the response
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    __attribute__((pure)) encoding::json TransactionToJSON(const Legacy::Transaction& tx, const TAO::Ledger::BlockState& block,
+                                     const uint32_t nVerbose = 0);
 
 
-        /** ContractToJSON
-         *
-         *  Converts a serialized contract stream to formattted JSON
-         *
-         *  @param[in] contract The contract to de-serialize
-         *  @param[in] nContract the id of the contract within the transaction
-         *  @param[in] nVerbose The verbose output level.
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json ContractToJSON(const TAO::Operation::Contract& contract,
-                                      const uint32_t nContract, const uint32_t nVerbose = 0);
+    /** ContractToJSON
+     *
+     *  Converts a serialized contract stream to formattted JSON
+     *
+     *  @param[in] contract The contract to de-serialize
+     *  @param[in] nContract the id of the contract within the transaction
+     *  @param[in] nVerbose The verbose output level.
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    __attribute__((pure)) encoding::json ContractToJSON(const TAO::Operation::Contract& contract,
+                                                        const uint32_t nContract, const uint32_t nVerbose = 0);
 
-        /** ObjectToJSON
-         *
-         *  Converts an Object Register to formattted JSON with no external lookups
-         *
-         *  @param[in] object The Object Register to convert
-         *  @param[in] hashRegister The address of the object.
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json ObjectToJSON(const TAO::Register::Object& object, const uint256_t& hashRegister = 0);
-
-
-        /** ObjectToJSON
-         *
-         *  Converts an Object Register to formattted JSON
-         *
-         *  @param[in] params The paramets passed in the request
-         *  @param[in] object The Object Register to convert
-         *  @param[in] hashRegister The register address of the object
-         *  @param[in] fLookupName True if the Name object record should be looked up
-         *
-         *  @return the formatted JSON object
-         *
-         **/
-        encoding::json ObjectToJSON(const encoding::json& params,
-                                const TAO::Register::Object& object,
-                                const TAO::Register::Address& hashRegister,
-                                bool fLookupName = true);
+    /** ObjectToJSON
+     *
+     *  Converts an Object Register to formattted JSON with no external lookups
+     *
+     *  @param[in] object The Object Register to convert
+     *  @param[in] hashRegister The address of the object.
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    __attribute__((pure)) encoding::json ObjectToJSON(const TAO::Register::Object& object, const uint256_t& hashRegister = 0);
 
 
-        /** StatementToJSON
-         *
-         *  Recursive helper function for QueryToJSON to recursively generate JSON statements for use with filters.
-         *
-         *  @param[out] vWhere The statements to parse into query.
-         *  @param[out] nIndex The current statement being processed.
-         *  @param[out] jStatement The statement reference to pass through recursion.
-         *
-         *  @return The JSON statement generated with query.
-         *
-         **/
-        encoding::json StatementToJSON(std::vector<std::string> &vWhere, uint32_t &nIndex, encoding::json &jStatement);
+    /** ObjectToJSON
+     *
+     *  Converts an Object Register to formattted JSON
+     *
+     *  @param[in] params The paramets passed in the request
+     *  @param[in] object The Object Register to convert
+     *  @param[in] hashRegister The register address of the object
+     *  @param[in] fLookupName True if the Name object record should be looked up
+     *
+     *  @return the formatted JSON object
+     *
+     **/
+    encoding::json ObjectToJSON(const encoding::json& params,
+                            const TAO::Register::Object& object,
+                            const TAO::Register::Address& hashRegister,
+                            bool fLookupName = true);
 
 
-        /** QueryToJSON
-         *
-         *  Turns a where query string in url encoding into a formatted JSON object.
-         *
-         *  @param[in] strWhere The string to parse and generate into json.
-         *
-         *  @return The JSON object generated with query.
-         *
-         **/
-        encoding::json QueryToJSON(const std::string& strWhere);
+    /** StatementToJSON
+     *
+     *  Recursive helper function for QueryToJSON to recursively generate JSON statements for use with filters.
+     *
+     *  @param[out] vWhere The statements to parse into query.
+     *  @param[out] nIndex The current statement being processed.
+     *  @param[out] jStatement The statement reference to pass through recursion.
+     *
+     *  @return The JSON statement generated with query.
+     *
+     **/
+    __attribute__((pure)) encoding::json StatementToJSON(std::vector<std::string> &vWhere, uint32_t &nIndex, encoding::json &jStatement);
 
 
-        /** ClauseToJSON
-         *
-         *  Turns a where clause string in url encoding into a formatted JSON object.
-         *
-         *  @param[in] strClause The string to parse and generate into json.
-         *
-         *  @return The JSON object generated with query.
-         *
-         **/
-        encoding::json ClauseToJSON(const std::string& strClause);
+    /** QueryToJSON
+     *
+     *  Turns a where query string in url encoding into a formatted JSON object.
+     *
+     *  @param[in] strWhere The string to parse and generate into json.
+     *
+     *  @return The JSON object generated with query.
+     *
+     **/
+    __attribute__((pure)) encoding::json QueryToJSON(const std::string& strWhere);
 
 
-        /** ParamsToJSON
-         *
-         *  Turns parameters from url encoding into a formatted JSON object.
-         *
-         *  @param[in] vParams The string to parse and generate into json.
-         *
-         *  @return The JSON object generated with query.
-         *
-         **/
-        encoding::json ParamsToJSON(const std::vector<std::string>& vParams);
+    /** ClauseToJSON
+     *
+     *  Turns a where clause string in url encoding into a formatted JSON object.
+     *
+     *  @param[in] strClause The string to parse and generate into json.
+     *
+     *  @return The JSON object generated with query.
+     *
+     **/
+    __attribute__((pure)) encoding::json ClauseToJSON(const std::string& strClause);
 
-    }
+
+    /** ParamsToJSON
+     *
+     *  Turns parameters from url encoding into a formatted JSON object.
+     *
+     *  @param[in] vParams The string to parse and generate into json.
+     *
+     *  @return The JSON object generated with query.
+     *
+     **/
+    __attribute__((pure)) encoding::json ParamsToJSON(const std::vector<std::string>& vParams);
 }
