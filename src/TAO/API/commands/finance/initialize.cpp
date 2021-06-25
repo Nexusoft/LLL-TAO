b3544a56c238e6e9b7330b6b49b85966dd078378
VidereLicet <colin@nexus.io>
2021-06-25 15:07:36 -0700

    Add list/trustaccounts deprecation method and alternative endpoint it was replaced by

--- src/TAO/API/commands/finance/initialize.cpp
+++ src/TAO/API/commands/finance/initialize.cpp
@@ -88,20 +88,6 @@ namespace TAO::API
             )
         );
 
-        /* Handle for all LIST operations. */
-        mapFunctions["list/balances"] = Function
-        (
-            std::bind
-            (
-                &Finance::List,
-                this,
-                std::placeholders::_1,
-                std::placeholders::_2
-            )
-            , version::get_version(5, 1, 0)
-            , "please use finance/get/balances instead"
-        );
-
         /* Handle for BALANCES. */
         mapFunctions["get/balances"] = Function
         (
@@ -132,5 +118,33 @@ namespace TAO::API
         mapFunctions["set/stake"]       = Function(std::bind(&Finance::SetStake, this, std::placeholders::_1, std::placeholders::_2));
 
         //mapFunctions["list/trustaccounts"] = Function(std::bind(&Finance::TrustAccounts, this, std::placeholders::_1, std::placeholders::_2));
+
+        /* DEPRECATED */
+        mapFunctions["list/balances"] = Function
+        (
+            std::bind
+            (
+                &Finance::List,
+                this,
+                std::placeholders::_1,
+                std::placeholders::_2
+            )
+            , version::get_version(5, 1, 0)
+            , "please use finance/get/balances instead"
+        );
+
+        /* DEPRECATED */
+        mapFunctions["list/trustaccounts"] = Function
+        (
+            std::bind
+            (
+                &Finance::List,
+                this,
+                std::placeholders::_1,
+                std::placeholders::_2
+            )
+            , version::get_version(5, 1, 0)
+            , "please use register/list/trust instead"
+        );
     }
 }
