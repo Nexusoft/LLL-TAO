9cbf1bf76350d6d8ba0702dd75d34693f480c365
VidereLicet <colin@nexus.io>
2021-07-16 16:00:17 -0700

    Complete ledger/get/block cleanup reducing required code and improving functionality. Allow filtering fieldname for this command

--- src/TAO/API/commands/ledger/get-block.cpp
+++ src/TAO/API/commands/ledger/get-block.cpp
@@ -11,24 +11,16 @@
 
 ____________________________________________________________________________________________*/
 
-#include <LLC/include/eckey.h>
-
 #include <LLD/include/global.h>
 
 #include <TAO/API/include/check.h>
 #include <TAO/API/include/extract.h>
-#include <TAO/API/include/global.h>
+#include <TAO/API/include/filter.h>
 #include <TAO/API/include/json.h>
 
 #include <TAO/API/types/commands/ledger.h>
 
 #include <TAO/Ledger/include/chainstate.h>
-#include <TAO/Ledger/types/tritium.h>
-#include <TAO/Ledger/include/create.h>
-#include <TAO/Ledger/types/state.h>
-
-#include <Util/include/hex.h>
-#include <Util/include/string.h>
 
 /* Global TAO namespace. */
 namespace TAO::API
@@ -36,62 +28,50 @@ namespace TAO::API
     /* Retrieves the block data for a given hash or height. */
     encoding::json Ledger::GetBlock(const encoding::json& jParams, const bool fHelp)
     {
-        if(config::fClient.load())
-            throw Exception(-300, "API can only be used to lookup data for the currently logged in signature chain when running in client mode");
-
-        /* Check for the block height parameter. */
-        if(jParams.find("hash") == jParams.end() && jParams.find("height") == jParams.end())
-            throw Exception(-84, "Missing hash or height");
-
-        /* Declare the BlockState to load from the DB */
-        TAO::Ledger::BlockState blockState;
-
-        /* look up by height*/
-        if(jParams.find("height") != jParams.end())
+        /* Check for height parameter */
+        TAO::Ledger::BlockState tLastBlock;
+        if(CheckParameter(jParams, "height", "string, number"))
         {
             /* Check that the node is configured to index blocks by height */
             if(!config::GetBoolArg("-indexheight"))
                 throw Exception(-85, "getblock by height requires the daemon to be started with the -indexheight flag.");
 
             /* Convert the incoming height string to an int*/
-            const uint32_t nHeight = ExtractInteger<uint32_t>(jParams, "height");
+            const uint32_t nHeight =
+                ExtractInteger<uint32_t>(jParams, "height");
 
             /* Check that the requested height is within our chain range*/
             if(nHeight > TAO::Ledger::ChainState::nBestHeight.load())
-                throw Exception(-82, "Block number out of range.");
+                throw Exception(-60, "[height] out of range [", TAO::Ledger::ChainState::nBestHeight.load(), "]");
 
             /* Read the block state from the the ledger DB using the height index */
-            if(!LLD::Ledger->ReadBlock(nHeight, blockState))
+            if(!LLD::Ledger->ReadBlock(nHeight, tLastBlock))
                 throw Exception(-83, "Block not found");
         }
-        else if(jParams.find("hash") != jParams.end())
+
+        /* Check for block-hash parameter. */
+        else if(CheckParameter(jParams, "hash", "string"))
         {
-            uint1024_t blockHash;
-            /* Load the hash from the jParams*/
-            blockHash.SetHex(jParams["hash"].get<std::string>());
-
-            /* Read the block state from the the ledger DB using the hash index */
-            if(!LLD::Ledger->ReadBlock(blockHash, blockState))
+            /* Get our starting block hash. */
+            if(!LLD::Ledger->ReadBlock(uint1024_t(jParams["hash"].get<std::string>()), tLastBlock))
                 throw Exception(-83, "Block not found");
         }
 
-        std::string strVerbose = "default";
-        if(jParams.find("verbose") != jParams.end())
-            strVerbose = jParams["verbose"].get<std::string>();
+        /* If no parameters are supplied, do a short regular random-read list. */
+        else
+            throw Exception(-56, "Missing Parameter [hash or height]");
 
         /* Default to verbosity 1 which includes only the hash */
-        uint32_t nVerbose = 1;
-        if(strVerbose == "none")
-            nVerbose = 0;
-        else if(strVerbose == "default")
-            nVerbose = 1;
-        else if(strVerbose == "summary")
-            nVerbose = 2;
-        else if(strVerbose == "detail")
-            nVerbose = 3;
+        const uint32_t nVerbose =
+            ExtractVerbose(jParams, 1);
 
-        encoding::json ret = TAO::API::BlockToJSON(blockState, nVerbose);
+        /* Build our response JSON. */
+        encoding::json jRet =
+            TAO::API::BlockToJSON(tLastBlock, nVerbose);
 
-        return ret;
+        /* Filter our fieldnames. */
+        FilterFieldname(jParams, jRet);
+
+        return jRet;
     }
 }
