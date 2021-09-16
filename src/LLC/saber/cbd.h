c6799af17475dbefc56b5903973849bba5fa07c9
VidereLicet <colin@nexus.io>
2021-09-16 12:05:55 -0700

    Adjust the PQ library to use for SABER with reference implementation

--- src/LLC/saber/cbd.h
+++ src/LLC/saber/cbd.h
@@ -1,17 +1,15 @@
+/*---------------------------------------------------------------------
+This file has been adapted from the implementation 
+(available at, Public Domain https://github.com/pq-crystals/kyber) 
+of "CRYSTALS – Kyber: a CCA-secure module-lattice-based KEM"
+by : Joppe Bos, Leo Ducas, Eike Kiltz, Tancrede Lepoint, 
+Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
+----------------------------------------------------------------------*/
 #ifndef CBD_H
 #define CBD_H
 
-/*---------------------------------------------------------------------
-This file has been adapted from the implementation
-(available at, Public Domain https://github.com/pq-crystals/kyber)
-of "CRYSTALS – Kyber: a CCA-secure module-lattice-based KEM"
-by : Joppe Bos, Leo Ducas, Eike Kiltz, Tancrede Lepoint,
-Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
-----------------------------------------------------------------------*/
-
-#include "poly.h"
 #include <stdint.h>
 
-void PQCLEAN_SABER_CLEAN_cbd(uint16_t *r, const unsigned char *buf);
+void cbd(uint16_t s[SABER_N], const uint8_t buf[SABER_POLYCOINBYTES]);
 
 #endif
