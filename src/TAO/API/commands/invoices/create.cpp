e07b374200be4119c614a72b7f872f8c3b27a1d1
VidereLicet <colin@nexus.io>
2022-07-27 22:36:30 -0700

    Add description key for each invoice item if supplied in items array

--- src/TAO/API/commands/invoices/create.cpp
+++ src/TAO/API/commands/invoices/create.cpp
@@ -107,23 +107,30 @@ namespace TAO::API
         uint64_t nTotal = 0;
         for(auto it = jItems.begin(); it != jItems.end(); ++it)
         {
+            /* Get a copy of our parameters. */
+            const encoding::json jItem = (*it);
+
             /* The item Unit Amount */
             const uint64_t nUnitAmount =
-                ExtractAmount((*it), nDigits);
+                ExtractAmount(jItem, nDigits);
 
             /* The item number of units */
             const uint64_t nUnits =
-                ExtractInteger<uint64_t>((*it), "units");
+                ExtractInteger<uint64_t>(jItem, "units");
 
             /* Rebuild our JSON to use correct formatting. */
-            const encoding::json jItem =
+            encoding::json jNew =
             {
                 { "amount",      FormatBalance(nUnitAmount, nDigits) },
                 { "units",       nUnits }
             };
 
+            /* Check for description key. */
+            if(CheckParameter(jItem, "description", "string"))
+                jNew["description"] = jItem["description"].get<std::string>();
+
             /* Now add to our new items list. */
-            jInvoice["items"].push_back(jItem);
+            jInvoice["items"].push_back(jNew);
 
             /* Aggregate our total balance now. */
             nTotal += (nUnitAmount * nUnits);
