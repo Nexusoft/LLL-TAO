3d6f91d8ed329014e0a3c2cf7facbdd3149fda30
VidereLicet <colin@nexus.io>
2022-09-02 20:11:27 -0700

    Update system/get/metrics to handle -indexaddress command so we can use the new indexes that were generated on the reindexing

--- src/TAO/API/commands/system/metrics.cpp
+++ src/TAO/API/commands/system/metrics.cpp
@@ -61,95 +61,210 @@ namespace TAO
             uint64_t nTotalObjects = 0;
             uint64_t nTotalTokenized = 0;
 
-            /* Batch read all trust keys. */
-            std::vector<TAO::Register::Object> vTrust;
-            if(LLD::Register->BatchRead("trust", vTrust, -1))
+            /* Special handle if address indexed. */
+            if(config::GetBoolArg("-indexaddress"))
             {
-                /* Check through all trust accounts. */
-                for(auto& object : vTrust)
+                /* Batch read all trust keys. */
+                std::vector<std::pair<uint256_t, TAO::Register::Object>> vTrust;
+                if(LLD::Register->BatchRead("trust_address", vTrust, -1))
                 {
-                    /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
-                    if(!object.Parse())
-                        continue;
+                    /* Check through all trust accounts. */
+                    for(auto& object : vTrust)
+                    {
+                        /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
+                        if(!object.second.Parse())
+                            continue;
 
-                    /* Increment total register. */
-                    ++nTotalRegisters;
+                        /* Increment total register. */
+                        ++nTotalRegisters;
 
-                    /* Check stake value over 0. */
-                    if(object.get<uint64_t>("stake") == 0)
-                        continue;
+                        /* Check stake value over 0. */
+                        if(object.second.get<uint64_t>("stake") == 0)
+                            continue;
 
-                    /* Update stake amount. */
-                    nTotalStake += object.get<uint64_t>("stake");
-                    nTotalTrust += object.get<uint64_t>("trust");
-                    nTotalTrustKeys++;
+                        /* Update stake amount. */
+                        nTotalStake += object.second.get<uint64_t>("stake");
+                        nTotalTrust += object.second.get<uint64_t>("trust");
+                        nTotalTrustKeys++;
+                    }
+                }
+            }
+            else
+            {
+                /* Batch read all trust keys. */
+                std::vector<TAO::Register::Object> vTrust;
+                if(LLD::Register->BatchRead("trust", vTrust, -1))
+                {
+                    /* Check through all trust accounts. */
+                    for(auto& object : vTrust)
+                    {
+                        /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
+                        if(!object.Parse())
+                            continue;
+
+                        /* Increment total register. */
+                        ++nTotalRegisters;
+
+                        /* Check stake value over 0. */
+                        if(object.get<uint64_t>("stake") == 0)
+                            continue;
+
+                        /* Update stake amount. */
+                        nTotalStake += object.get<uint64_t>("stake");
+                        nTotalTrust += object.get<uint64_t>("trust");
+                        nTotalTrustKeys++;
+                    }
                 }
             }
 
-            /* Batch read all names. */
-            std::vector<TAO::Register::Object> vNames;
-            if(LLD::Register->BatchRead("name", vNames, -1))
+
+
+            /* Special handle if address indexed. */
+            if(config::GetBoolArg("-indexaddress"))
             {
-                /* Check through all names. */
-                for(auto& object : vNames)
+                /* Batch read all names. */
+                std::vector<std::pair<uint256_t, TAO::Register::Object>> vNames;
+                if(LLD::Register->BatchRead("name_address", vNames, -1))
                 {
-                    /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
-                    if(!object.Parse())
-                        continue;
+                    /* Check through all names. */
+                    for(auto& object : vNames)
+                    {
+                        /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
+                        if(!object.second.Parse())
+                            continue;
 
-                    /* Increment total register. */
-                    ++nTotalRegisters;
+                        /* Increment total register. */
+                        ++nTotalRegisters;
 
-                    /* global */
-                    if(object.get<std::string>("namespace") == TAO::Register::NAMESPACE::GLOBAL)
-                        nTotalGlobalNames++;
+                        /* global */
+                        if(object.second.get<std::string>("namespace") == TAO::Register::NAMESPACE::GLOBAL)
+                            nTotalGlobalNames++;
 
-                    /* namespaced */
-                    else if(object.get<std::string>("namespace") != "")
-                        nTotalNamespacedNames ++;
+                        /* namespaced */
+                        else if(object.second.get<std::string>("namespace") != "")
+                            nTotalNamespacedNames ++;
 
-                    /* Update count*/
-                    nTotalNames++;
+                        /* Update count*/
+                        nTotalNames++;
+                    }
+                }
+            }
+            else
+            {
+                /* Batch read all names. */
+                std::vector<TAO::Register::Object> vNames;
+                if(LLD::Register->BatchRead("name", vNames, -1))
+                {
+                    /* Check through all names. */
+                    for(auto& object : vNames)
+                    {
+                        /* Skip over invalid objects (THIS SHOULD NEVER HAPPEN). */
+                        if(!object.Parse())
+                            continue;
+
+                        /* Increment total register. */
+                        ++nTotalRegisters;
+
+                        /* global */
+                        if(object.get<std::string>("namespace") == TAO::Register::NAMESPACE::GLOBAL)
+                            nTotalGlobalNames++;
+
+                        /* namespaced */
+                        else if(object.get<std::string>("namespace") != "")
+                            nTotalNamespacedNames ++;
+
+                        /* Update count*/
+                        nTotalNames++;
+                    }
                 }
             }
 
-            /* Batch read all object registers. */
-            std::vector<TAO::Register::Object> vObjects;
-            if(LLD::Register->BatchRead("object", vObjects, -1))
+
+
+            /* Special handle if address indexed. */
+            if(config::GetBoolArg("-indexaddress"))
             {
-                /* Check through all names. */
-                for(auto& object : vObjects)
+                /* Batch read all object registers. */
+                std::vector<std::pair<uint256_t, TAO::Register::Object>> vObjects;
+                if(LLD::Register->BatchRead("object_address", vObjects, -1))
                 {
-                    /* Increment total register. */
-                    ++nTotalRegisters;
+                    /* Check through all names. */
+                    for(auto& object : vObjects)
+                    {
+                        /* Increment total register. */
+                        ++nTotalRegisters;
 
-                    /* Check if tokenized*/
-                    if(TAO::Register::Address(object.hashOwner).IsToken())
-                        nTotalTokenized++;
+                        /* Check if tokenized*/
+                        if(TAO::Register::Address(object.second.hashOwner).IsToken())
+                            nTotalTokenized++;
 
-                    /* Update count*/
-                    nTotalObjects++;
+                        /* Update count*/
+                        nTotalObjects++;
+                    }
+                }
+            }
+            else
+            {
+                /* Batch read all object registers. */
+                std::vector<TAO::Register::Object> vObjects;
+                if(LLD::Register->BatchRead("object", vObjects, -1))
+                {
+                    /* Check through all names. */
+                    for(auto& object : vObjects)
+                    {
+                        /* Increment total register. */
+                        ++nTotalRegisters;
+
+                        /* Check if tokenized*/
+                        if(TAO::Register::Address(object.hashOwner).IsToken())
+                            nTotalTokenized++;
+
+                        /* Update count*/
+                        nTotalObjects++;
+                    }
                 }
             }
 
             /* Check by unique owners. */
             std::set<uint256_t> setOwners;
 
-            /* Batch read all object registers. */
-            std::vector<TAO::Register::Object> vCrypto;
-            if(LLD::Register->BatchRead("crypto", vCrypto, -1))
+            /* Special handle if address indexed. */
+            if(config::GetBoolArg("-indexaddress"))
             {
-                /* Check through all names. */
-                for(auto& rObject : vCrypto)
+                /* Batch read all object registers. */
+                std::vector<std::pair<uint256_t, TAO::Register::Object>> vCrypto;
+                if(LLD::Register->BatchRead("crypto_address", vCrypto, -1))
                 {
-                    /* Increment total register. */
-                    ++nTotalRegisters;
+                    /* Check through all names. */
+                    for(auto& rObject : vCrypto)
+                    {
+                        /* Increment total register. */
+                        ++nTotalRegisters;
 
-                    /* Use a set to get unique owners. */
-                    setOwners.insert(rObject.hashOwner);
-                    ++nTotalCrypto;
+                        /* Use a set to get unique owners. */
+                        setOwners.insert(rObject.second.hashOwner);
+                        ++nTotalCrypto;
+                    }
+
                 }
+            }
+            else
+            {
+                /* Batch read all object registers. */
+                std::vector<TAO::Register::Object> vCrypto;
+                if(LLD::Register->BatchRead("crypto", vCrypto, -1))
+                {
+                    /* Check through all names. */
+                    for(auto& rObject : vCrypto)
+                    {
+                        /* Increment total register. */
+                        ++nTotalRegisters;
 
+                        /* Use a set to get unique owners. */
+                        setOwners.insert(rObject.hashOwner);
+                        ++nTotalCrypto;
+                    }
+                }
             }
 
 
