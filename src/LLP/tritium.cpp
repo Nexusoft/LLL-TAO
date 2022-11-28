0cd0dbe3c884f64bbc717416493863070e65bcc0
VidereLicet <colin@nexus.io>
2022-11-27 19:56:06 -0700

    Add more filters to ACTION::LIST to break out when we have reached the best block

--- src/LLP/tritium.cpp
+++ src/LLP/tritium.cpp
@@ -1328,8 +1328,13 @@ namespace LLP
                                 /* Loop through all available states. */
                                 //for(auto& state : vStates)
                                 {
+                                    /* Break if we fail to read our block. */
                                     if(!LLD::Ledger->ReadBlock(hashStart, state))
-                                        return debug::drop(NODE, "failed to read starting block");
+                                        return debug::drop(NODE, "failed to read ", hashStart.SubString());
+
+                                    /* Break if we are the best chain. */
+                                    if(state.hashNextBlock == 0)
+                                        break;
 
                                     /* Update start every iteration. */
                                     hashStart = state.hashNextBlock;
