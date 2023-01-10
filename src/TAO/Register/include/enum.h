5f32b5b3087db545cee5a4cde7e95821748b648b
VidereLicet <colin@nexus.io>
2023-01-09 21:33:08 -0700

    Change enum values for OBJECT types to not interfere with the same types for REGISTER enum. Since none of these values hit the disk and are used for return values for Standard() and Base() functions we can adjust, but all values that hit the disk such as REGISTER values cannot be modified without an update to the consensus rules

--- src/TAO/Register/include/enum.h
+++ src/TAO/Register/include/enum.h
@@ -96,25 +96,25 @@ namespace TAO
             enum : uint8_t
             {
                 /* Non-Standard Object (User Defined Type). */
-                NONSTANDARD  = 0x01,
+                NONSTANDARD  = 0x11,
 
                 /* Account Object Register. */
-                ACCOUNT      = 0x02,
+                ACCOUNT      = 0x12,
 
                 /* Token Object Register. */
-                TOKEN        = 0x03,
+                TOKEN        = 0x13,
 
                 /* Trust Object Register. */
-                TRUST        = 0x04,
+                TRUST        = 0x14,
 
                 /* Name Object Register. */
-                NAME         = 0x05,
+                NAME         = 0x15,
 
                 /* Namespace Object Register. */
-                NAMESPACE    = 0x06,
+                NAMESPACE    = 0x16,
 
                 /* Crypto Object Register. */
-                CRYPTO       = 0x07
+                CRYPTO       = 0x17
             };
         };
 
