2324ddb234e9b46ba96d4ebbaac89c9b958373d6
VidereLicet <colin@nexus.io>
2023-04-11 20:32:58 -0700

    Bump version to release candidate 16 for 5.1.0-rc16 beta.

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -27,7 +27,7 @@ namespace version
     const uint32_t CLIENT_PATCH = 0;
 
     /* Build version determines release candidate. */
-    const uint32_t CLIENT_REVISION = 15;
+    const uint32_t CLIENT_REVISION = 16;
 
 
     /* The version of the actual wallet client. */
