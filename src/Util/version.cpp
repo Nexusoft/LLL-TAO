62932deffe61ce11092f2ecfc7360749ee7cceff
VidereLicet <colin@nexus.io>
2023-10-20 17:05:25 -0700

    Bump release candidate to rc8 to indicate using the new SSL acceptor code.

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -27,7 +27,7 @@ namespace version
     const uint32_t CLIENT_PATCH = 1;
 
     /* Build version determines release candidate. */
-    const uint32_t CLIENT_REVISION = 7;
+    const uint32_t CLIENT_REVISION = 8;
 
     /* This tells us minor build tweaks rather than release candidate. */
     const uint32_t CLIENT_BUILD    = 0;
