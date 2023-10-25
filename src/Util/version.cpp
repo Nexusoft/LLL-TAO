ab2f587fa04c06dab2e07ccdbc6e39760d24ed96
VidereLicet <colin@nexus.io>
2023-10-25 15:21:42 -0700

    Bump release candidate to rc13 for new recovery system updates.

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -27,7 +27,7 @@ namespace version
     const uint32_t CLIENT_PATCH = 1;
 
     /* Build version determines release candidate. */
-    const uint32_t CLIENT_REVISION = 12;
+    const uint32_t CLIENT_REVISION = 13;
 
     /* This tells us minor build tweaks rather than release candidate. */
     const uint32_t CLIENT_BUILD    = 0;
