1432ea096051c0f50fe6fad9a1216702391aa8c2
VidereLicet <colin@nexus.io>
2025-10-25 04:48:03 -0700

    Add additional parameters handling unique genesis calls for the VERIFY::NEXTHASH script. Add additional Ledger Level VM byte code to cover most of our cases. Build an object register with a function to interact both of the new VM boilerplate with one another.

--- tests/live/main.cpp
+++ tests/live/main.cpp
@@ -45,18 +45,35 @@ int main(int argc, char** argv)
 
         //single signature authentication
         ssAuthorization << AUTH::ENABLE_IF << uint16_t(OP::ALL);
-        ssAuthorization << AUTH::TYPES::NEXTHASH << hashFalcon;
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << hashFalcon;
+        ssAuthorization << AUTH::OP::THEN;
+
         ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(OP::ALL);
         ssAuthorization << AUTH::ENDIF;
 
 
 
 
-        //multisignature authentication
+        //multisignature authentication with user generated credentials
         ssAuthorization << AUTH::ENABLE_IF << uint16_t(OP::ALL);
-        ssAuthorization << AUTH::TYPES::NEXTHASH << hashFalcon;
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << hashFalcon;
         ssAuthorization << AUTH::OP::AND;
-        ssAuthorization << AUTH::TYPES::NEXTHASH << hashFalcon2;
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << hashFalcon2;
+        ssAuthorization << AUTH::OP::THEN;
+
+        ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(OP::ALL);
+        ssAuthorization << AUTH::ENDIF;
+
+
+
+        //multisignature authentication with multiple sigchains
+        uint256_t hashGenesis2, hashGenesis3;
+        ssAuthorization << AUTH::ENABLE_IF << uint16_t(OP::ALL);
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << AUTH::PARAM::GENESIS << hashGenesis2 << AUTH::CALLER::CRYPTO::SIGN;
+        ssAuthorization << AUTH::OP::AND;
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << AUTH::PARAM::GENESIS << hashGenesis3 << AUTH::CALLER::CRYPTO::SIGN;
+        ssAuthorization << AUTH::OP::THEN;
+
         ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(OP::ALL);
         ssAuthorization << AUTH::ENDIF;
 
@@ -65,13 +82,16 @@ int main(int argc, char** argv)
 
         //staking and credit only, no unstake coins
         ssAuthorization << AUTH::ENABLE_IF << uint16_t(TRUST::ENABLED | CREDIT::ENABLED);
-        ssAuthorization << AUTH::TYPES::NEXTHASH << hashStaking;
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << hashStaking;
+        ssAuthorization << AUTH::OP::THEN;
 
         //check that we are not removing stake with these credentials
         ssAuthorization << AUTH::OP::IF;
         ssAuthorization << AUTH::TRANSACTION::OP << AUTH::OP::EQUALS << TAO::Operation::OP::TRUST;
         ssAuthorization << AUTH::OP::AND;
         ssAuthorization << AUTH::TRUST::PARAM::CHANGE << AUTH::OP::LESSTHAN << AUTH::TYPES::INT64 << int64_t(0);
+        ssAuthorization << AUTH::OP::THEN;
+
         ssAuthorization << AUTH::RETURN << AUTH::DENIED;
         ssAuthorization << AUTH::ENDIF;
 
@@ -85,17 +105,80 @@ int main(int argc, char** argv)
         ssAuthorization << AUTH::ENABLE_IF << uint16_t(INVOKE::ENABLED);
 
         //we can use this access pattern to allow any external sigchain to invoke a function in any object register we have
-        ssAuthorization << AUTH::TYPES::NEXTHASH << AUTH::CALLER::CRYPTO::SIGN; //this is a 256 bit hash of any caller
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << AUTH::CALLER::CRYPTO::SIGN; //this is a 256 bit hash of any caller
+        ssAuthorization << AUTH::OP::THEN;
 
         //default exit from authorization routine
         ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(INVOKE::ENABLED);
         ssAuthorization << AUTH::ENDIF;
 
+
+
+        //system authentication for processing methods for augmented contracts for a single individual
+        uint256_t hashGenesis;
+        ssAuthorization << AUTH::ENABLE_IF << uint16_t(INVOKE::ENABLED);
+
+        //we can use this access pattern to allow any external sigchain to invoke a function in any object register we have
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << AUTH::CALLER::CRYPTO::SIGN; //this is a 256 bit hash of any caller
+
+        //only allow invoke from a specific sigchain
+        ssAuthorization << AUTH::OP::AND;
+        ssAuthorization << AUTH::CALLER::GENESIS << AUTH::OP::EQUALS << AUTH::TYPES::UINT256 << hashGenesis;
+        ssAuthorization << AUTH::OP::THEN;
+
+        ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(INVOKE::ENABLED);
+        ssAuthorization << AUTH::ENDIF;
+
+
+
+        //system authentication for processing methods for augmented contracts for a single individual with single operation code
+        ssAuthorization << AUTH::ENABLE_IF << uint16_t(INVOKE::ENABLED);
+
+        //by adding param genesis in here we can skip the checks for auth::caller::genesis and integrate in the single call
+        ssAuthorization << AUTH::VERIFY::NEXTHASH << AUTH::PARAM::GENESIS << hashGenesis << AUTH::CALLER::CRYPTO::SIGN;
+        ssAuthorization << AUTH::OP::THEN;
+
+        ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(INVOKE::ENABLED);
+        ssAuthorization << AUTH::ENDIF;
+
+
+
         //recovery authentication
         ssAuthorization << AUTH::ENABLE_IF << uint16_t(OP::ALL);
         ssAuthorization << AUTH::TYPES::RECOVERY << AUTH::TYPES::FALCON << hashFalcon;
+        ssAuthorization << AUTH::OP::THEN;
+
         ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(OP::NONE);
         ssAuthorization << AUTH::ENDIF;
+
+
+
+        //multisig recovery authentication with two recovery phrases (can wipe entire ledger VM script except own recovery hash)
+        ssAuthorization << AUTH::ENABLE_IF << uint16_t(OP::ALL);
+        ssAuthorization << AUTH::TYPES::RECOVERY << hashRecovery1;
+        ssAuthorization << AUTH::OP::AND;
+        ssAuthorization << AUTH::TYPES::RECOVERY << hashRecovery2;
+        ssAuthorization << AUTH::OP::THEN;
+
+        ssAuthorization << AUTH::RETURN << AUTH::GRANTED << uint16_t(OP::NONE);
+        ssAuthorization << AUTH::ENDIF;
+    }
+
+
+    {
+        //let's make some functions now for an object register
+
+        TAO::Register::Object account;
+
+        /* Generate the object register values. */
+        account << std::string("balance")       << uint8_t(TYPES::MUTABLE)   << uint8_t(TYPES::UINT64_T) << uint64_t(0)
+                << std::string("token")         << uint8_t(TYPES::UINT256_T) << uint256_t(0);
+
+        //let us now create a function of binary data.
+        account << std::string("total") << uint8_t(TYPES::UINT64_T) << uint64_t(0);
+
+        TAO::Operation::Stream ssMethod;
+        account << std::string("::DEBIT") << uint8_t(TYPES::METHOD) << ssMethod.Bytes();
     }
 
     return 0;
