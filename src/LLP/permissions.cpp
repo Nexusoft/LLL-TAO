ec7ad1409f38290ec44ba9f51e2e6a0d23bce4ee
VidereLicet <colin@nexus.io>
2026-03-30 02:24:20 -0700

    Don't check FLAGS::MEMPOOL for the coinbase transaction on a given block, improve our CheckPermissions code to ensure that it is clean and that we can rely on it at large scale.

--- src/LLP/permissions.cpp
+++ src/LLP/permissions.cpp
@@ -21,8 +21,15 @@ ________________________________________________________________________________
 
 
 /*  IP Filtering Definitions. IP's are Filtered By Ports. */
-bool CheckPermissions(const std::string &strAddress, const uint16_t nPort)
+bool CheckPermissions(const std::string& strAddress, const uint16_t nPort)
 {
+    /* Build some constants if we need them. */
+    static const uint16_t TRITIUM_MAINNET_PORT_CHECK     = config::GetArg(std::string("-port"),    TRITIUM_MAINNET_PORT);
+    static const uint16_t TRITIUM_MAINNET_SSL_PORT_CHECK = config::GetArg(std::string("-sslport"), TRITIUM_MAINNET_SSL_PORT);
+
+    /* Make a const copy of our IP filters for easy access. */
+    static const std::map<uint16_t, std::vector<std::string>> mapFilters = config::mapIPFilters;
+
     /* Bypass localhost addresses first. */
     if(strAddress == "127.0.0.1" || strAddress == "::1") //XXX: we may not want this rule, assess security
         return true;
@@ -37,30 +44,33 @@ bool CheckPermissions(const std::string &strAddress, const uint16_t nPort)
 
     /* Determine whether or not the current port is open by default, or closed requiring an llpallowip whitelist.
      * Ports open by default can also use a whitelist, and will no longer be treated as open for other addresses */
-    bool fOpen = false;
+    bool fStandardPort = false;
     if(config::fTestNet.load()) //XXX: icky, this should be cleaned up here
-    {
-        /* Testnet ports open only for testnet */
-        if(nPort == static_cast<uint16_t>(config::GetArg(std::string("-serverport"), (TRITIUM_TESTNET_PORT + (config::GetArg("-testnet", 0) - 1))))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-serversslport"), (TRITIUM_TESTNET_SSL_PORT + (config::GetArg("-testnet", 0) - 1))))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-port"), (TRITIUM_TESTNET_PORT + (config::GetArg("-testnet", 0) - 1))))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-sslport"), (TRITIUM_TESTNET_SSL_PORT + (config::GetArg("-testnet", 0) - 1))))
-            || nPort == static_cast<uint16_t>(TESTNET_TIME_LLP_PORT)
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-p2pport"), TESTNET_P2P_PORT ))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-p2psslport"), TESTNET_P2P_SSL_PORT )))
-            fOpen = true;
-    }
+        fStandardPort = true;
     else
     {
-        /* Mainnet ports open only for mainnet */
-        if(nPort == static_cast<uint16_t>(config::GetArg(std::string("-serverport"), TRITIUM_MAINNET_PORT))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-serversslport"), TRITIUM_MAINNET_SSL_PORT))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-port"), TRITIUM_MAINNET_PORT))
-            || nPort == static_cast<uint16_t>(config::GetArg(std::string("-sslport"), TRITIUM_MAINNET_SSL_PORT))
-            || nPort == static_cast<uint16_t>(MAINNET_TIME_LLP_PORT)
-            || nPort == 80
-            || nPort == 443) //these two ports are for http server traffic that should always be up if enabled.
-            fOpen = true;
+        /* Use a switch statement to check each of our ports. */
+        switch(nPort)
+        {
+            /* Handle the mainnet port. */
+            case MAINNET_TIME_LLP_PORT:
+                fStandardPort = true;
+                break;
+
+            /* Handle the hard coded ports here. */
+            case 80:
+            case 443:
+                fStandardPort = true;
+                break;
+
+            default:
+                fStandardPort = false;
+                break;
+        }
+
+        /* We need to use this because we can't evaluate a constexpr in the switch statement from our static const variables. */
+        if(nPort == TRITIUM_MAINNET_PORT_CHECK || nPort == TRITIUM_MAINNET_SSL_PORT_CHECK)
+            fStandardPort = true;
     }
 
     /* Check for lookup ports. */
@@ -68,11 +78,11 @@ bool CheckPermissions(const std::string &strAddress, const uint16_t nPort)
         return true;
 
     /* If no llpallowip whitelist defined for a default open port then we assume permission */
-    if(config::mapIPFilters[nPort].size() == 0 && fOpen)
+    if(mapFilters.at(nPort).empty() && fStandardPort)
         return true;
 
     /* Check against the llpallowip list from config / commandline parameters. */
-    for(const auto& strIPFilter : config::mapIPFilters[nPort])
+    for(const auto& strIPFilter : mapFilters.at(nPort))
     {
         /* Split the components of the IP so that we can check for wildcard ranges. */
         std::vector<std::string> vCheck;
@@ -86,6 +96,7 @@ bool CheckPermissions(const std::string &strAddress, const uint16_t nPort)
         bool fIPMatches = true;
         for(uint8_t nByte = 0; nByte < 4; ++nByte)
         {
+            /* Check that the address matches with wildcards. */
             if(vCheck[nByte] != "*" && vCheck[nByte] != vAddress[nByte])
                 fIPMatches = false;
         }
