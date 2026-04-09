500c7ae6e44a634d4a0f48e2ecd94e7952ae739b
VidereLicet <colin@nexus.io>
2026-04-09 03:16:56 -0700

    Bump release candidate to 5.1.6-rc5 build 1

--- src/Util/version.cpp
+++ src/Util/version.cpp
@@ -27,10 +27,10 @@ namespace version
     const uint32_t CLIENT_PATCH = 6;
 
     /* Build version determines release candidate. */
-    const uint32_t CLIENT_REVISION = 4;
+    const uint32_t CLIENT_REVISION = 5;
 
     /* This tells us minor build tweaks rather than release candidate. */
-    const uint32_t CLIENT_BUILD    = 3;
+    const uint32_t CLIENT_BUILD    = 1;
 
 
     /* The version of the actual wallet client. */
